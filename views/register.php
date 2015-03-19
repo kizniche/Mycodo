e568360227fc05765cc2d49125d684a206e9aac2
kizniche <kyletgabriel@gmail.com>
2015-03-19 12:12:40 -0400

    Changed styling

--- views/register.php
+++ views/register.php
@@ -1,38 +1,69 @@
+<html>
+<head>
+<title>Register</title>
+</head>
+<body>
 <?php
-// show potential errors / feedback (from registration object)
+// Show potential errors
 if (isset($registration)) {
     if ($registration->errors) {
         foreach ($registration->errors as $error) {
             echo $error;
         }
+    echo '<p>';
     }
     if ($registration->messages) {
         foreach ($registration->messages as $message) {
             echo $message;
         }
+    echo '<p>';
     }
 }
 ?>
 
-<!-- register form -->
 <form method="post" action="register.php" name="registerform">
-
-    <!-- the user name input field uses a HTML5 pattern check -->
-    <label for="login_input_username">Username (only letters and numbers, 2 to 64 characters)</label>
-    <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
-
-    <!-- the email input field uses a HTML5 email type check -->
-    <label for="login_input_email">User's email</label>
-    <input id="login_input_email" class="login_input" type="email" name="user_email" required />
-
-    <label for="login_input_password_new">Password (min. 6 characters)</label>
-    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
-
-    <label for="login_input_password_repeat">Repeat password</label>
-    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
-    <input type="submit"  name="register" value="Register" />
-
+<table>
+    <tr>
+        <td>
+            <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
+        </td>
+        <td>
+            <!-- the user name input field uses a HTML5 pattern check -->
+            <label for="login_input_username">Username (letters/numbers, 2 to 64 characters)</label>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input id="login_input_email" class="login_input" type="email" name="user_email" required />
+        </td>
+        <td>
+            <!-- the email input field uses a HTML5 email type check -->
+            <label for="login_input_email">User's email</label>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
+        </td>
+        <td>
+            <label for="login_input_password_new">Password (min. 6 characters)</label>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
+        </td>
+        <td>
+            <label for="login_input_password_repeat">Repeat password</label>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="submit"  name="register" value="Register" />
+        </td>
+    </tr>
+</table>
 </form>
-
-<!-- backlink -->
-<a href="index.php">Back to Login Page</a>
+<p><a href="index.php">Back to Login Page</a></p>
+</body>
+</html>
