bc271d7eb5870a989daa144b65867105647e2f48
kizniche <kyletgabriel@gmail.com>
2015-03-19 11:30:55 -0400

    Set up MySQL database for php-login

---
+++ source/php-login-mysql-install/02-create-and-fill-users-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `login`.`users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
+  `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name, unique',
+  `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
+  `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email, unique',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
