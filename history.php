8ab4724d00eb7daf48750a3a025ec9dcd60f2d23
kizniche <kyletgabriel@gmail.com>
2015-03-20 13:14:56 -0400

    Added auth.log and relay.log to log viewer

--- history.php
+++ history.php
@@ -8,7 +8,10 @@
 */
 
 ####### Configure #######
-$sensor_log = getcwd() . "/log/sensor.log";
+$cwd = getcwd();
+$sensor_log = $cwd . "/log/sensor.log";
+$relay_log = $cwd . "/log/relay.log";
+$auth_log = $cwd . "/log/auth.log";
 
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
     exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
@@ -23,28 +26,48 @@ $login = new Login();
 
 if ($login->isUserLoggedIn() == true) {
 
-echo '<html><head><META HTTP-EQUIV="refresh" CONTENT="65"></head><body>';
+    echo '<html><head><META HTTP-EQUIV="refresh" CONTENT="65"></head><body>';
+    echo '<FORM action="" method="POST">';
+    echo 'Lines: <input type="text" maxlength=8 size=8 name="Lines" /> ';
+    echo '<input type="submit" name="Sensor" value="Sensor"> ';
+    echo '<input type="submit" name="Relay" value="Relay"> ';
+    echo '<input type="submit" name="Auth" value="Auth">';
+    echo '</FORM>';
 
-if(isset($_POST['Lines'])) {
-	$Lines = $_POST['Lines'];
-	displayModeform();
+if(isset($_POST['Sensor'])) {
+    echo '<p>Year Mo Day Hour Min Sec Timestamp RH Tc Tf DPf DPc<p>';
+    if ($_POST['Lines'] != '') {
+        $Lines = $_POST['Lines'];
 	echo `tail -n $Lines $sensor_log | sed 's/$/<br>/'`;
-}
-else {
-	displayModeform();
+    } else {
 	echo `tail -n 20 $sensor_log | sed 's/$/<br>/'`;
+    }
+}
+
+if(isset($_POST['Relay'])) {
+    echo '<p>Year Mo Day Hour Min Sec R1Sec R2Sec R3Sec R4Sec</p>';
+    if ($_POST['Lines'] != '') {
+        $Lines = $_POST['Lines'];
+        echo `tail -n $Lines $relay_log | sed 's/$/<br>/'`;
+    } else {
+        echo `tail -n 20 $relay_log | sed 's/$/<br>/'`;
+    }
+}
+
+if(isset($_POST['Auth'])) {
+    echo '<p>Time, Type of auth, user, IP, Hostname, Referral, Browser</p>';
+    if ($_POST['Lines'] != '') {
+        $Lines = $_POST['Lines'];
+        echo `tail -n $Lines $auth_log | sed 's/$/<br>/'`;
+    } else {
+        echo `tail -n 20 $auth_log | sed 's/$/<br>/'`;
+    }
 }
 
 echo '</body></html>';
 
 } else {
         include("views/not_logged_in.php");
-}
-
-function displayModeform() {
-        echo "<FORM action=\"\" method=\"POST\">";
-        echo "Lines: <input type=\"text\" maxlength=8 size=8 name=\"Lines\" /> ";
-        echo "<input type=\"submit\" name=\"SubmitMode\" value=\"Set\"></FORM><p>Year Mo Day Hour Min Sec Timestamp RH Tc Tf DPf DPc<p>";
 }
 
 ?>
