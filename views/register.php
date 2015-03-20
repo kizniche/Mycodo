434e38f143996d1d0adac2d7d9bdd7483635e2cf
kizniche <kyletgabriel@gmail.com>
2015-03-20 02:37:49 -0400

    Layout changes

--- views/register.php
+++ views/register.php
@@ -5,18 +5,18 @@
 <form method="post" action="register.php" name="registerform">
     <label for="user_name"><?php echo WORDING_REGISTRATION_USERNAME; ?></label>
     <input id="user_name" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
-
+    <p>
     <label for="user_email"><?php echo WORDING_REGISTRATION_EMAIL; ?></label>
     <input id="user_email" type="email" name="user_email" required />
-
+    <p>
     <label for="user_password_new"><?php echo WORDING_REGISTRATION_PASSWORD; ?></label>
     <input id="user_password_new" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
-
+    <p>
     <label for="user_password_repeat"><?php echo WORDING_REGISTRATION_PASSWORD_REPEAT; ?></label>
     <input id="user_password_repeat" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
-
+    <p>
     <img src="tools/showCaptcha.php" alt="captcha" />
-
+    <p>
     <label><?php echo WORDING_REGISTRATION_CAPTCHA; ?></label>
     <input type="text" name="captcha" required />
 
