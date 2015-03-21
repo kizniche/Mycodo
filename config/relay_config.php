510afba8c93aa334b4563c92fff31e6206030fd6
kizniche <kyletgabriel@gmail.com>
2015-03-20 20:01:47 -0400

    config file to save name and pin of relay

---
+++ config/relay_config.php
@@ -0,0 +1,17 @@
+<?php
+    /* Relay Configuratio file
+    *  Define relay names and pins
+    */
+
+    $relay = array
+    (
+	array("Relay1", "HEPA", 4),
+	array("Relay2", "HUMI", 3),
+	array("Relay3", "CFAN", 1),
+	array("Relay4", "HEAT", 0),
+	array("Relay5", "REL5", 0),
+	array("Relay6", "REL6", 0),
+	array("Relay7", "REL7", 0),
+	array("Relay8", "REL8", 0)
+    );
+?>
