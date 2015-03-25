1b30732356ba069d754d01253d9cca3a94c0196e
kizniche <kyletgabriel@gmail.com>
2015-03-25 00:23:35 -0400

    Script to set GPIO mode to output and set all HIGH (relays off for this particular relay board)

---
+++ cgi-bin/GPIO-initialize.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+import ConfigParser
+
+config_file = '/var/www/mycodo/config/mycodo.cfg'
+
+# GPIO pins (BCM numbering) and name of devices attached to relay
+relayPin = [0] * 9
+relayName = [0] * 9
+
+def ReadCfg():
+    global relayPin
+
+    config = ConfigParser.RawConfigParser()
+    config.read(config_file)
+
+    relayPin[1] = config.getint('RelayPins', 'relay1pin')
+    relayPin[2] = config.getint('RelayPins', 'relay2pin')
+    relayPin[3] = config.getint('RelayPins', 'relay3pin')
+    relayPin[4] = config.getint('RelayPins', 'relay4pin')
+    relayPin[5] = config.getint('RelayPins', 'relay5pin')
+    relayPin[6] = config.getint('RelayPins', 'relay6pin')
+    relayPin[7] = config.getint('RelayPins', 'relay7pin')
+    relayPin[8] = config.getint('RelayPins', 'relay8pin')
+
+ReadCfg()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(relayPin[1], GPIO.OUT)
+GPIO.setup(relayPin[2], GPIO.OUT)
+GPIO.setup(relayPin[3], GPIO.OUT)
+GPIO.setup(relayPin[4], GPIO.OUT)
+GPIO.setup(relayPin[5], GPIO.OUT)
+GPIO.setup(relayPin[6], GPIO.OUT)
+GPIO.setup(relayPin[7], GPIO.OUT)
+GPIO.setup(relayPin[8], GPIO.OUT)
+
+GPIO.output(relayPin[1], 1)
+GPIO.output(relayPin[2], 1)
+GPIO.output(relayPin[3], 1)
+GPIO.output(relayPin[4], 1)
+GPIO.output(relayPin[5], 1)
+GPIO.output(relayPin[6], 1)
+GPIO.output(relayPin[7], 1)
+GPIO.output(relayPin[8], 1)
