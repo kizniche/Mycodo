8ccb0b1f6d6ff320e08c787d0ba5eda046d68724
kizniche <kyletgabriel@gmail.com>
2015-06-14 16:00:48 -0400

    Update copyright notices to GPLv3

--- image.php
+++ image.php
@@ -1,10 +1,26 @@
 <?php
 /*
+*  image.php - displays images when image directory is protected by the
+*              web server
 *
-*  image.php - displays images when image directory is protected by the web server
-*  By Kyle Gabriel
-*  2012 - 2015
+*  Copyright (C) 2015  Kyle T. Gabriel
 *
+*  This file is part of Mycodo
+*
+*  Mycodo is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  Mycodo is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with Mycodo. If not, see <http://www.gnu.org/licenses/>.
+*
+*  Contact at kylegabriel.com
 */
 
 ####### Configure #######
