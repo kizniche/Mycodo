aa65b94725c4fb91571f758f58f3010e8d73b7ac
kizniche <kyletgabriel@gmail.com>
2015-07-01 23:39:42 -0400

    Add v1.0 branch

---
+++ 1.0/www/graph/auth.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+$User = "user";
+$Password = "82d2d9ca4f73f8af2db84735634cb550";
+
+switch ($_GET['id']) {
+case hash:
+  echo "<html><head><title></title></head><body><style>body,td,input { font-family: verdana; font-size: 8pt; }</style>";
+  if($error) echo "<p><b>Wrong credentials.</b></p>";
+  echo "<form action=\"\" method=\"post\"><table width='500' border=0><tr>";
+  echo "<td>password:</td><td><input type='password' name='md5password'></td><td>";
+  if($_POST['md5password']) echo md5($_POST['md5password']);
+  echo "</td></tr><tr><td colspan='2'><input type='Submit' value='Get md5&raquo;' name='loginbutton'></td></tr></table></form></body></html>";
+  exit;
+break;
+
+default:
+  if(!$_SESSION['authenticated'])
+    if($_POST['loginbutton']) {
+      $inputuser = $_POST['input_user'];
+      $input_Password = hashPassword($_POST['input_password']);
+
+      if(!strcmp($inputuser ,$User) && !strcmp($input_Password ,$Password)) {
+        $_SESSION['authenticated'] = 1;
+        if ($_GET['r'] == 1) header("Location:".$_SERVER[PHP_SELF]."?r=1");
+        else header("Location:".$_SERVER[PHP_SELF]);
+      }
+      else displayform(1);
+    }
+    else displayform(0);
+}
+
+function hashPassword($password) {
+  $salt1 = "df+-+-u87^GhuiuVcc.</#;jhdgOJeEehd>IOJ~1838HB>Ihssf\}\{jhggd";
+  $salt2 = "Jq*92@/<hX S_=!`/dJhFDtay@7369chsl[\?.><tqcw>wiejrgGQF`~1112";
+  //encrypt the password, rotate characters by length of original password
+  $len = strlen($password);
+  $password = md5($password);
+  $password = rotateHEX($password,$len);
+  return md5($salt1.$password.salt2);
+}
+
+function rotateHEX($string, $n) {
+  //for more security, randomize this string
+  $chars="abcdef1234567890";
+  $str="";
+  for ($i=0;$i<strlen($string);$i++)
+  {
+    $pos = strpos($chars,$string[$i]);
+    $pos += $n;
+    if ($pos>=strlen($chars))
+      $pos = $pos % strlen($chars);
+    $str.=$chars[$pos];
+  }
+  return $str;
+}
+
+function displayform($error) {
+  echo "<html><head><title></title></head><body><style>body,td,input { font-family: verdana; font-size: 8pt; }</style>";
+  if($error) echo "<p><b>Wrong credentials.</b></p>";
+  echo "<form action=\"\" method=\"post\"><table width='400' border=0><tr><td width='100'>username:</td>";
+  echo "<td><input type='text' name='input_user'></td></tr><tr><td>password:</td><td><input type='password' name='input_password'></td></tr>";
+  echo "<tr><td colspan='2'><input type='Submit' value='Login&raquo;' name='loginbutton'></td></tr></table></form></body></html>";
+  exit;
+}
+?>
