b515fa1d242919209d683f78d6cefceb5de32119
kizniche <kyletgabriel@gmail.com>
2015-08-09 22:25:47 -0400

    update to mycodo-wrapper

--- 3.5/cgi-bin/mycodo-wrapper.c
+++ 3.5/cgi-bin/mycodo-wrapper.c
@@ -1,5 +1,8 @@
+#include <linux/limits.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -8,10 +11,19 @@ int main(int argc, char *argv[]) {
 	char cmd[255];
 
 	if (argc > 1) {
-		sprintf(cmd, "/etc/init.d/mycodo %s", argv[1]);
-		system(cmd);
+		if (argv[1] == "stop" || argv[1] == "start" ||
+			argv[1] == "restart" || argv[1] == "debug") {
+			sprintf(cmd, "/etc/init.d/mycodo %s", argv[1]);
+			system(cmd);
+		} else if (strcmp(argv[1], "update") == 0) {
+			char updateScript[255];
+			strncpy(updateScript, argv[0], sizeof(updateScript));
+			dirname(updateScript);
+			strncat(updateScript, "/../update-mycodo.sh", sizeof(updateScript));
+			system(updateScript);
+		}
 	} else {
-		printf("The command had no other arguments.\n");
+		printf("mycodo-wrapper: Allows web interface to stop and start the daemon and update the mycodo system to the latest version.\n");
 	}
 
 	return 0;
