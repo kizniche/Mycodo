e8b9e51969c78196de1edb182e88a500ffce2792
kizniche <kyletgabriel@gmail.com>
2015-09-28 10:11:12 -0400

    Use /var/Mycodo-backups instead of home directory (too difficult to determine which user the backups are under when attempting to create restore feature

--- 3.5/update-mycodo.sh
+++ 3.5/update-mycodo.sh
@@ -50,10 +50,10 @@ case "${1:-''}" in
 
                 NOW=$(date +"%Y-%m-%d_%H-%M-%S")
                 CURCOMMIT=$(git rev-parse --short HEAD)
-                printf "#### Creating backup Mycodo-backups/Mycodo-$NOW-$CURCOMMIT ####\n"
-                mkdir -p $DIR/../../Mycodo-backups
-                mkdir -p $DIR/../../Mycodo-backups/Mycodo-$NOW-$CURCOMMIT
-                cp -a $DIR/../../Mycodo/3.5/. $DIR/../../Mycodo-backups/Mycodo-$NOW-$CURCOMMIT/
+                printf "#### Creating backup /var/Mycodo-backups/Mycodo-$NOW-$CURCOMMIT ####\n"
+                mkdir -p /var/Mycodo-backups
+                mkdir -p /var/Mycodo-backups/Mycodo-$NOW-$CURCOMMIT
+                cp -a $DIR/../../Mycodo/3.5/. /var/Mycodo-backups/Mycodo-$NOW-$CURCOMMIT/
 
                 printf "#### Updating from github ####\n"
                 git fetch --all
