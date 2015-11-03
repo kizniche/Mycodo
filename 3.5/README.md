f01c3868cbdd5619528855d4209b680903da7f2e
Kizniche <KyleTGabriel@gmail.com>
2015-11-03 18:15:40 -0500

    v3.5.86: Change the PID period to a constant interval (thanks @KnyazSh) and add framework for executing custom Python code in the daemon

--- 3.5/README.md
+++ 3.5/README.md
@@ -90,6 +90,9 @@ This is an experimental branch of mycodo. It is undergoing constant changes and 
 
 Major changes for each versioned release
 
+#### 3.5.86
++ Change the PID period to a constant interval (thanks @KnyazSh)"
+
 #### 3.5.85
 + New client-side graph generation using HighCharts/HighStock
 
--- 3.5/includes/mycodo.php
+++ 3.5/includes/mycodo.php
@@ -22,7 +22,7 @@
 *  Contact at kylegabriel.com
 */
 
-$version = "3.5.85";
+$version = "3.5.86";
 
 ######### Start Edit Configure #########
 
@@ -440,7 +440,7 @@ if (isset($output_error)) {
                                 <option value="12h">12 Hours</option>
                                 <option value="1d">1 Day</option>
                                 <option value="3d">3 Days</option>
-                                <option value="1w">1 Week</option>
+                                <option value="1w" selected>1 Week</option>
                                 <option value="2w">2 Weeks</option>
                                 <option value="1m">1 Month</option>
                                 <option value="3m">3 Months</option>
@@ -681,7 +681,7 @@ if (isset($output_error)) {
                     <table class="conditional">
                         <tr>
                             <td>
-                                Conditional Statements &nbsp;<span style="font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
+                                Conditional Statements<br/><span style="padding-top: 0.5em;font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
                             </td>
                         </tr>
                     </table>
@@ -750,7 +750,7 @@ if (isset($output_error)) {
                         for ($z = 0; $z < count($conditional_relay_id); $z++) {
                         ?>
                         <tr>
-                            <td style="padding-top: 1em; padding-left: 1em;">
+                            <td style="padding-top: 1em; padding-left: 1em; white-space: nowrap;">
                             <?php
                             echo '<button type="submit" name="DeleteRelay' . $z . 'Conditional" title="Delete conditional statement">Delete</button> ';
 
@@ -805,7 +805,7 @@ if (isset($output_error)) {
                             }
 
                             echo '</tr>';
-                        } 
+                        }
                     ?>
                     <tr>
                         <td style="padding-top: 1em;"></td>
@@ -813,10 +813,37 @@ if (isset($output_error)) {
                     </tr>
                     </table>
                     </form>
+
+                    <table class="conditional">
+                        <tr>
+                            <td>
+                                Custom Code<br/><span style="padding-top: 0.5em; font-size: 0.7em;">Warning: This is a very powerful feature. If the syntax is not completely correct, it will fail. If you haven't read the <a class="manual" href="manual.html#custom-code" target="_blank">Custom Code</a> section of the manual and understood everything, do not attempt to use this feature.</span>
+                            </td>
+                        </tr>
+                    </table>
+
+                    <form action="?tab=sensor<?php if (isset($_GET['r']))  echo '&r=' , $_GET['r']; ?>" method="POST">
+                    <table class="conditional" style="width: 100%;">
+                        <tr>
+                            <td style="padding-left: 1em;">Setup Code</td>
+                            <td style="padding-left: 1em;">Main Loop Code</td>
+                        </tr>
+                        <tr>
+                            <td style="padding-left: 1em;">
+                                <textarea style="width: 100%;" rows="4" maxlength="1000" name="conditional_advanced_setup" title=""></textarea>
+                            </td>
+                            <td style="padding-left: 1em;">
+                                <textarea style="width: 100%;" rows="4" maxlength="1000" name="conditional_advanced_main" title=""></textarea>
+                            </td>
+                            <td>
+                                <button type="submit" style="height:5em; width:4em;" style="height:5em;" name="AddRelayConditional" title="Save new relay conditional statement">Save</button>
+                            </td>
+                        </tr>
+                    </table>
+                    </form>
                     <?php
                     }
                     ?>
-
                 </div>
             <?php
             }
@@ -1142,7 +1169,7 @@ if (isset($output_error)) {
                 <table class="conditional">
                     <tr>
                         <td>
-                            Conditional Statements &nbsp;<span style="font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
+                            Conditional Statements<br/><span style="padding-top: 0.5em;font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
                         </td>
                     </tr>
                 </table>
@@ -1201,7 +1228,7 @@ if (isset($output_error)) {
                         for ($z = 0; $z < count($conditional_t_id[$i]); $z++) {
                     ?>
                     <tr>
-                        <td style="padding-top: 1em; padding-left: 1em;">
+                        <td style="padding-top: 1em; padding-left: 1em; white-space: nowrap;">
                             <?php
                             echo '<button type="submit" name="DeleteT' . $i . '-' . $z . 'Conditional" title="Delete conditional statement">Delete</button> ';
                             if ($conditional_t_state[$i][$z]) {
@@ -1636,7 +1663,7 @@ if (isset($output_error)) {
                 <table class="conditional">
                     <tr>
                         <td>
-                            Conditional Statements &nbsp;<span style="font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
+                            Conditional Statements<br/><span style="padding-top: 0.5em;font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
                         </td>
                     </tr>
                 </table>
@@ -1700,7 +1727,7 @@ if (isset($output_error)) {
                         for ($z = 0; $z < count($conditional_ht_id[$i]); $z++) {
                     ?>
                     <tr>
-                        <td style="padding-top: 1em; padding-left: 1em;">
+                        <td style="padding-top: 1em; padding-left: 1em; white-space: nowrap;">
                             <?php
                             echo '<button type="submit" name="DeleteHT' . $i . '-' . $z . 'Conditional" title="Delete conditional statement">Delete</button> ';
                             if ($conditional_ht_state[$i][$z]) {
@@ -2020,7 +2047,7 @@ if (isset($output_error)) {
                 <table class="conditional">
                     <tr>
                         <td>
-                            Conditional Statements &nbsp;<span style="font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
+                            Conditional Statements<br/><span style="padding-top: 0.5em;font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
                         </td>
                     </tr>
                 </table>
@@ -2079,7 +2106,7 @@ if (isset($output_error)) {
                         for ($z = 0; $z < count($conditional_co2_id[$i]); $z++) {
                     ?>
                     <tr>
-                        <td style="padding-top: 1em; padding-left: 1em;">
+                        <td style="padding-top: 1em; padding-left: 1em; white-space: nowrap;">
                             <?php
                             echo '<button type="submit" name="DeleteCO2' . $i . '-' . $z . 'Conditional" title="Delete conditional statement">Delete</button> ';
                             if ($conditional_co2_state[$i][$z]) {
@@ -2491,7 +2518,7 @@ if (isset($output_error)) {
                 <table class="conditional">
                     <tr>
                         <td>
-                            Conditional Statements &nbsp;<span style="font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
+                            Conditional Statements<br/><span style="padding-top: 0.5em;font-size: 0.7em;">Note: Ensure these conditional statements don't produce conflicts with themselves or interfere with running PID controllers.</span>
                         </td>
                     </tr>
                 </table>
@@ -2554,7 +2581,7 @@ if (isset($output_error)) {
                         for ($z = 0; $z < count($conditional_press_id[$i]); $z++) {
                     ?>
                     <tr>
-                        <td style="padding-top: 1em; padding-left: 1em;">
+                        <td style="padding-top: 1em; padding-left: 1em; white-space: nowrap;">
                             <?php
                             echo '<button type="submit" name="DeletePress' . $i . '-' . $z . 'Conditional" title="Delete conditional statement">Delete</button> ';
                             if ($conditional_press_state[$i][$z]) {
