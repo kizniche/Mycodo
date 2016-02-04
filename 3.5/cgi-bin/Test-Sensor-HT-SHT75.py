963819128c726bd36c0918162be77165266101b8
Seraphim of Marouda <seraphim.of.marouda@gmail.com>
2016-02-04 12:23:08 +0200

    Added support for a more precise internally build DEW-Point calculator (output)

--- 3.5/cgi-bin/Test-Sensor-HT-SHT75.py
+++ 3.5/cgi-bin/Test-Sensor-HT-SHT75.py
@@ -9,8 +9,10 @@ sht = Sht(4, 17)
 for x in range(0, 10):
 
     temperature = sht.read_t()
-    humidity = sht.read_rh()
+    humidity = sht.read_rh(temperature)
+    dew_point = sht.read_dew_point(temperature, humidity)
 
     print "Temperature: %s" % temperature
     print "Humidity: %s" % humidity
+    print "Dew Point: " % dew_point
     time.sleep(2)
