65986efda4ed27a477b62753fbc2ecd4211c583e
Kizniche <KyleTGabriel@gmail.com>
2016-04-11 16:50:05 -0400

    Add exception-handling to DS18B20 reads

--- 3.5/mycodo_core/mycodo.py
+++ 3.5/mycodo_core/mycodo.py
@@ -2251,26 +2251,29 @@ def read_t(sensor_id, device, pin):
         rpi_tempf = float(rpi_temp_file.read())
         temperature = rpi_tempf / 1000  # Temperature in Celsius
     elif device in 'DS18B20':
-        import glob
-        os.system('modprobe w1-gpio')
-        os.system('modprobe w1-therm')
-        base_dir = '/sys/bus/w1/devices/'
-        #device_folder = glob.glob(base_dir + '28*')[0]
-        device_file = base_dir + '28-' + pin + '/w1_slave'
-        
-        def read_temp_raw():
-            f = open(device_file, 'r')
-            lines = f.readlines()
-            f.close()
-            return lines
-        lines = read_temp_raw()
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
+        try:
+            import glob
+            os.system('modprobe w1-gpio')
+            os.system('modprobe w1-therm')
+            base_dir = '/sys/bus/w1/devices/'
+            #device_folder = glob.glob(base_dir + '28*')[0]
+            device_file = base_dir + '28-' + pin + '/w1_slave'
+            
+            def read_temp_raw():
+                f = open(device_file, 'r')
+                lines = f.readlines()
+                f.close()
+                return lines
             lines = read_temp_raw()
-        equals_pos = lines[1].find('t=')
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos + 2:]
-            temperature = float(temp_string) / 1000.0
+            while lines[0].strip()[-3:] != 'YES':
+                time.sleep(0.2)
+                lines = read_temp_raw()
+            equals_pos = lines[1].find('t=')
+            if equals_pos != -1:
+                temp_string = lines[1][equals_pos + 2:]
+                temperature = float(temp_string) / 1000.0
+        except Exception as msg:
+            logging.debug("[Read T Sensor-%s] Could not read DS18B20: %s", sensor_id + 1, msg)
     else:
         logging.debug("[Read T Sensor-%s] Device not recognized: %s", sensor_id + 1, device)
 
