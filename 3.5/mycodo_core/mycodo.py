cb43f39eb8ccfa5614a1e111adbd12229b820287
Kizniche <KyleTGabriel@gmail.com>
2016-04-12 21:22:47 -0400

    New method to read DS18B20 sensor (hopefully fixes an issue with disconnects)

--- 3.5/mycodo_core/mycodo.py
+++ 3.5/mycodo_core/mycodo.py
@@ -52,6 +52,7 @@ from lockfile import LockFile
 from rpyc.utils.server import ThreadedServer
 from tentacle_pi.AM2315 import AM2315
 from sht_sensor import Sht # sht senserion support
+from w1thermsensor import W1ThermSensor
 
 install_directory = os.path.dirname(os.path.abspath(__file__)) + "/.."
 
@@ -2252,26 +2253,7 @@ def read_t(sensor_id, device, pin):
         temperature = rpi_tempf / 1000  # Temperature in Celsius
     elif device in 'DS18B20':
         try:
-            import glob
-            os.system('modprobe w1-gpio')
-            os.system('modprobe w1-therm')
-            base_dir = '/sys/bus/w1/devices/'
-            #device_folder = glob.glob(base_dir + '28*')[0]
-            device_file = base_dir + '28-' + pin + '/w1_slave'
-            
-            def read_temp_raw():
-                f = open(device_file, 'r')
-                lines = f.readlines()
-                f.close()
-                return lines
-            lines = read_temp_raw()
-            while lines[0].strip()[-3:] != 'YES':
-                time.sleep(0.2)
-                lines = read_temp_raw()
-            equals_pos = lines[1].find('t=')
-            if equals_pos != -1:
-                temp_string = lines[1][equals_pos + 2:]
-                temperature = float(temp_string) / 1000.0
+            temperature = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, pin).get_temperature()
         except Exception as msg:
             logging.debug("[Read T Sensor-{}] Could not read DS18B20: {}".format(sensor_id + 1, msg))
     else:
