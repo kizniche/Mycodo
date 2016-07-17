6825d96b6d0f8afae73c85b5f2f9ba002c6fc507
Kizniche <KyleTGabriel@gmail.com>
2016-07-17 00:12:26 -0400

    Update install script

--- setup.sh
+++ setup.sh
@@ -36,8 +36,79 @@ NOW=$(date +"%m-%d-%Y %H:%M:%S")
 printf "### Mycodo installation began at $NOW\n\n"
 
 if [ -f $INSTALL_DIRECTORY/mycodo/scripts/update_mycodo.sh ]; then
-    $INSTALL_DIRECTORY/mycodo/scripts/update_mycodo.sh upgrade-packages &&
-    $INSTALL_DIRECTORY/mycodo/scripts/update_mycodo.sh setup &&
+    $INSTALL_DIRECTORY/mycodo/scripts/update_mycodo.sh upgrade-packages
+
+    printf "#### Installing prerequisites\n"
+    wget abyz.co.uk/rpi/pigpio/pigpio.zip -P $INSTALL_DIRECTORY/
+    unzip pigpio.zip
+    cd $INSTALL_DIRECTORY/PIGPIO
+    make -j4
+    make install
+
+    git clone git://git.drogon.net/wiringPi $INSTALL_DIRECTORY/wiringPi
+    cd $INSTALL_DIRECTORY/wiringPi
+    ./build
+
+    wget https://dl.influxdata.com/influxdb/releases/influxdb_0.13.0_armhf.deb -P $INSTALL_DIRECTORY/
+    dpkg -i $INSTALL_DIRECTORY/influxdb_0.13.0_armhf.deb
+    service influxdb start
+
+    cd $INSTALL_DIRECTORY
+    pip install -r requirements.txt --upgrade
+
+    rm -rf ./PIGPIO ./pigpio.zip ./wiringPi ./src ./influxdb_0.13.0_armhf.deb
+
+    sleep 2
+
+    printf "#### Creating InfluxDB database and user\n"
+    influx -execute "CREATE DATABASE mycodo_db"
+    influx -database mycodo_db -execute "CREATE USER mycodo WITH PASSWORD 'mmdu77sj3nIoiajjs'"
+
+    printf "#### Creating cron entry to start pigpiod at boot\n"
+    $INSTALL_DIRECTORY/mycodo/scripts/crontab.sh mycodo
+
+    printf "#### Installing and configuring apache2 web server\n"
+    apt-get install -y apache2 libapache2-mod-wsgi
+    a2enmod wsgi ssl
+    ln -sf $INSTALL_DIRECTORY/mycodo_flask_apache.conf /etc/apache2/sites-enabled/000-default.conf
+
+    printf "#### Creating SSL certificates at $INSTALL_DIRECTORY/mycodo/frontend/ssl_certs (replace with your own if desired)\n"
+    mkdir -p $INSTALL_DIRECTORY/mycodo/frontend/ssl_certs
+    cd $INSTALL_DIRECTORY/mycodo/frontend/ssl_certs/
+
+    openssl req \
+        -new \
+        -x509 \
+        -sha512 \
+        -days 365 \
+        -nodes \
+        -out cert.pem \
+        -keyout privkey.pem\
+        -subj "/C=US/ST=Georgia/L=Atlanta/O=mycodo/OU=mycodo/CN=mycodo"
+
+    openssl genrsa -out certificate.key 1024
+
+    openssl req \
+        -new \
+        -key certificate.key \
+        -out certificate.csr \
+        -subj "/C=US/ST=Georgia/L=Atlanta/O=mycodo/OU=mycodo/CN=mycodo"
+
+    openssl x509 -req \
+        -days 365 \
+        -in certificate.csr -CA cert.pem \
+        -CAkey privkey.pem \
+        -set_serial $RANDOM \
+        -out chain.pem
+
+    rm -f certificate.csr
+
+    printf "#### Enabling mycodo startup script\n"
+    systemctl enable $INSTALL_DIRECTORY/mycodo/scripts/mycodo.service
+
+    printf "#### Creating SQLite databases\n"
+    $INSTALL_DIRECTORY/init_databases.py -i all
+
     $INSTALL_DIRECTORY/mycodo/scripts/update_mycodo.sh initialize
 else
     printf "Error: $INSTALL_DIRECTORY/mycodo/scripts/update_mycodo.sh not found\n"
