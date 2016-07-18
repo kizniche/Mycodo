e092db7a741ef3efbff74c017ea602f6babf74b9
Kizniche <KyleTGabriel@gmail.com>
2016-07-18 17:33:23 -0400

    Add Dew Point to SHT sensor

--- mycodo/sensors/sht.py
+++ mycodo/sensors/sht.py
@@ -18,6 +18,7 @@ class SHT_read(object):
             sht_sensor = Sht(self.clock_pin, self.pin, voltage=self.voltage)
             self._temperature = sht_sensor.read_t()
             self._humidity = sht_sensor.read_rh()
+            self._dewpoint = sht.read_dew_point(self.temperature, self.humidity)
         except:
             return 1
 
@@ -28,6 +29,10 @@ class SHT_read(object):
     @property
     def humidity(self):
         return self._humidity
+
+    @property
+    def dewpoint(self):
+        return self._dewpoint
 
     @property
     def crc_check(self):
@@ -48,6 +53,7 @@ class SHT_read(object):
         response = {
             'humidity': self.humidity,
             'temperature': self.temperature,
+            'dewpoint': self.dew_point
         }
         return response
 
@@ -56,9 +62,10 @@ class SHT_read(object):
 
 
 if __name__ == "__main__":
-    sht = SHT(16, 17, 3.5)
+    sht = SHT_read(16, 17, 3.5)
 
     for measurement in sht:
         print("Temperature: {}".format(measurement['temperature']))
         print("Humidity: {}".format(measurement['humidity']))
+        print("Dew Point: {}".format(measurement['dewpoint']))
         time.sleep(1)
