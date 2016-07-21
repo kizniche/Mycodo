da792fccf132fe2d600352174eca15ab35f0c24c
Kizniche <KyleTGabriel@gmail.com>
2016-07-21 10:53:32 -0400

    Potential fix for dewpoint calculation and sensor measurement class, attempt 2

--- mycodo/sensors/sensorutils.py
+++ mycodo/sensors/sensorutils.py
@@ -5,17 +5,22 @@
 import math
 
 def dewpoint(t, rh):
-    dict_tn = dict(water=243.12, ice=272.62) # Table 9
-    dict_m = dict(water=17.62, ice=22.46) # Table 9
-    if t is None: t, rh = self.read_t(), None
-    if rh is None: rh = self.read_rh(t)
-    t_range = 'water' if t >= 0 else 'ice'
+    dict_tn = dict(water=243.12, ice=272.62)
+    dict_m = dict(water=17.62, ice=22.46)
+    if t is None:
+        t, rh = self.read_t(), None
+    if rh is None:
+        rh = self.read_rh(t)
+    if t >= 0:
+        t_range = 'water'
+    else:
+        t_range = 'ice'
     tn, m = dict_tn[t_range], dict_m[t_range]
     if rh/100.0 <= 0:
         # Cannot perform log on 0 or negative number
         return None
     else:
-        return (
+        return float(
             tn * (math.log(rh / 100.0) + (m * t) / (tn + t))
             / (m - math.log(rh / 100.0) - m * t / (tn + t)) )
 
