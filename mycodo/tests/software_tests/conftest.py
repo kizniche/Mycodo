4b68d1b1d8ec1e0d54fd890629df0c4b8475abdd
todd <todd.a.smith@me.com>
2016-10-25 21:39:41 -0700

    Reworked previous test fixtures to use new config objects.  updated db uri fixtures

--- mycodo/tests/software_tests/conftest.py
+++ mycodo/tests/software_tests/conftest.py
@@ -2,57 +2,74 @@
 """ pytest file """
 import pytest
 import logging
-from os.path import isfile
-from mycodo.config import SQL_DATABASE_MYCODO, SQL_DATABASE_NOTE, SQL_DATABASE_USER
-from init_databases import create_dbs
+import tempfile
+import shutil
+import os
 from mycodo.databases.utils import session_scope
 from mycodo.databases.users_db.models import Users
 
 
+def uri_to_path(uri):
+    """ splits a URI back into a path """
+    return str(uri).split('sqlite:///')[1]
+
+
 @pytest.yield_fixture()
-def user_db():
-    """ yields a session to the user db """
-    with session_scope("sqlite:///" + SQL_DATABASE_USER) as db_session:
-        yield db_session
-
-        db_session.commit()
-
-
-def setup_databases_if_missing():
+def tmp_file():
     """
-    Creates the databases if they are not present.
-
-    Why?  Various parts of this codebase interact with the databases as soon
-    as they are imported.  An example is the mycodo_flask file.  In order to
-    run tests, we need to have something for these database calls to
-    interact with.
-
-    Normally we would spin up a disposable in-memory sqlite database, but
-    only a single in-memory can be supported at any time.  This code base
-    requires all three databases to be loaded.
-
-    This function will eventually go away because there are much better
-    ways to do this.  It requires a major refactoring of the database
-    structure which will be finished shortly
-
-    :return: None
+    make a tmp file in an empty tmp dir and
+    remove it after it is used
     """
 
-    if not all([isfile(SQL_DATABASE_USER), isfile(SQL_DATABASE_NOTE), isfile(SQL_DATABASE_MYCODO)]):
-        logging.warning("--> Tests Delayed: Databases were missing and need to be installed."
-                        "  You will need to run your tests again")
-        create_dbs('', create_all=True)
+    parent_dir = tempfile.mkdtemp()
+    _, tmp_path = tempfile.mkstemp(dir=parent_dir)
 
-    # mycodo_flask exits if there is no user called admin. So we create one
-    with session_scope("sqlite:///" + SQL_DATABASE_USER) as db_session:
+    yield tmp_path
+
+    if os.path.isdir(parent_dir):
+        shutil.rmtree(parent_dir)
+
+
+@pytest.fixture()
+def db_config(tmp_file, mycodo_db_uri, user_db_uri, notes_db_uri):
+    """ Creates a config object to setup and databases during tests """
+    class Config(object):
+        SQL_DATABASE_USER = tmp_file
+        SQL_DATABASE_MYCODO = uri_to_path(mycodo_db_uri)
+        SQL_DATABASE_NOTE = uri_to_path(notes_db_uri)
+
+        MYCODO_DB_PATH = mycodo_db_uri
+        NOTES_DB_PATH = notes_db_uri
+        USER_DB_PATH = user_db_uri
+
+    return Config
+
+
+@pytest.fixture()
+def mycodo_db_uri(tmp_file):
+    """ returns the sqlalchemy URI as the MYCODO_DB_PATH """
+    return ''.join(['sqlite:///', tmp_file, '_mycodo_db'])
+
+
+@pytest.fixture()
+def user_db_uri(tmp_file):
+    """ returns the sqlalchemy URI as the USER_DB_PATH """
+    return ''.join(['sqlite:///', tmp_file, '_user_db'])
+
+
+@pytest.fixture()
+def notes_db_uri(tmp_file):
+    """ returns the sqlalchemy URI as the USER_DB_PATH """
+    return ''.join(['sqlite:///', tmp_file, '_notes_db'])
+
+
+def create_admin_user(user_db_uri):
+    """ mycodo_flask exits if there is no user called admin. So we create one """
+
+    with session_scope(user_db_uri) as db_session:
         if not db_session.query(Users).filter_by(user_restriction='admin').count():
-            logging.warning("--> Creating new 'test' user as an admin")
+            logging.info("--> Creating new 'test' user as an admin")
             db_session.add(Users(user_name='test', user_restriction='admin'))
             db_session.commit()
-
-
-@pytest.yield_fixture()
-def mycodo_db_sess():
-    """ yields mycodo_db sqlalchemy session """
-    with session_scope("""sqlite://""" + SQL_DATABASE_USER) as session:
-        yield session
+        else:
+            logging.warning("--> Dirty User DB: Admin user was already setup in: '{uri}'".format(uri=user_db_uri))
