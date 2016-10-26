997b5326da9cddf699907b386bd5d667c7c541db
todd <todd.a.smith@me.com>
2016-10-25 21:40:28 -0700

    Added assert message

--- mycodo/tests/software_tests/test_mycodo_flask/test_endpoints.py
+++ mycodo/tests/software_tests/test_mycodo_flask/test_endpoints.py
@@ -12,7 +12,7 @@ def redirects_to_login_page(app, endpoint):
     """ helper function that verifies that we see the login page """
     response = app.get(endpoint).maybe_follow()
     assert response.status_code == 200, "Endpoint Tested: {page}".format(page=endpoint)
-    assert "Mycodo Login" in response
+    assert "Mycodo Login" in response, 'Did Not see login page.  Saw "{body}"'.format(body=response.body)
 
 
 def test_daemon_active_for_non_logged_in_user(testapp):
