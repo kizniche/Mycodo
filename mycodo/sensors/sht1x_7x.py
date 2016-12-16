65933c75a128bdc1774426507cb90c0818feccaf
Kyle Gabriel <KyleTGabriel@gmail.com>
2016-12-16 14:41:03 -0500

    Cast string as float before testing is_integer()

--- mycodo/sensors/sht1x_7x.py
+++ mycodo/sensors/sht1x_7x.py
@@ -20,7 +20,7 @@ class SHT1x7xSensor(AbstractSensor):
         self._temperature = 0.0
         self.pin = pin
         self.clock_pin = clock_pin
-        if voltage.is_integer():
+        if float(voltage).is_integer():
             self.voltage = "{}V".format(int(voltage))
         else:
             self.voltage = "{}V".format(voltage)
