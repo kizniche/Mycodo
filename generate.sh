24b6d04223f7a583f0175d3af77738a8d5f49b23
kizniche <KyleTGabriel@gmail.com>
2017-03-30 00:46:37 -0400

    Fix references in generate.sh

--- generate.sh
+++ generate.sh
@@ -1,18 +1,21 @@
-#! /usr/bin/env bash
-
-# This BASH script generates different output files by calling pandoc
+#!/bin/bash
+# Generates the Mycodo Manual in different formats with pandoc
+#
+# To install dependencies on Debian/Ubuntu:
+# sudo apt-get install pandoc texlive texlive-xetex
+#
 
 # check if pandoc is installed
 if [[ `command -v pandoc` ]]
 then
-  # outputs the qubes cheat sheet as PDF file
+  # output PDF file
   pandoc metadata.yaml mycodo-manual.md --latex-engine=xelatex -s -o mycodo-manual.pdf
 
-  # outputs the qubes cheat sheet as HTML (HTML5) file
+  # output HTML (HTML5) file
   pandoc metadata.yaml mycodo-manual.md -s -S -t html5 -o mycodo-manual.html
   cp mycodo-manual.html /var/www/mycodo/mycodo/mycodo_flask/templates/manual.html
 
-  # outputs the qubes cheat sheet as plain text
+  # output plain text
   pandoc mycodo-manual.md -s -S -t plain -o mycodo-manual.txt
 else
   # pandoc is not installed inform the user
