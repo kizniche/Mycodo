f3bf1bf9384298f7e6e5ac07e1a61c393ae40344
kizniche <KyleTGabriel@gmail.com>
2017-03-30 01:30:17 -0400

    Update pandoc scripts location and make HTML standalone (all css contained in HTML file)

---
+++ mycodo/scripts/pandoc_generate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Generates the Mycodo Manual in different formats with pandoc
+#
+# To install dependencies on Debian/Ubuntu:
+# sudo apt-get install pandoc texlive texlive-xetex
+#
+
+INSTALL_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd -P )
+
+# check if pandoc is installed
+if [[ `command -v pandoc` ]]
+then
+  # output PDF file
+  pandoc ${INSTALL_DIRECTORY}/mycodo/scripts/pandoc_metadata.yaml ${INSTALL_DIRECTORY}/mycodo-manual.md --latex-engine=xelatex -s -o ${INSTALL_DIRECTORY}/mycodo-manual.pdf
+
+  # output HTML (HTML5) file
+  pandoc ${INSTALL_DIRECTORY}/mycodo-manual.md -H ${INSTALL_DIRECTORY}/mycodo/scripts/pandoc.css_style -s -S -t html5 -o ${INSTALL_DIRECTORY}/mycodo-manual.html
+  cp ${INSTALL_DIRECTORY}/mycodo-manual.html ${INSTALL_DIRECTORY}/mycodo/mycodo_flask/templates/manual.html
+
+  # output plain text
+  pandoc ${INSTALL_DIRECTORY}/mycodo-manual.md -s -S -t plain -o ${INSTALL_DIRECTORY}/mycodo-manual.txt
+else
+  # pandoc is not installed inform the user
+  echo "The command pandoc could not be found in the PATH!\n"
+fi
