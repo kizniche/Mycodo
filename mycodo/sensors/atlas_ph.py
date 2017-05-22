0de17649a680628cd4436a130f443b7076375479
Kyle Gabriel <kizniche@users.noreply.github.com>
2017-05-22 14:38:34 -0400

    Remove atlas sensor debug lines (#238)

--- mycodo/sensors/atlas_ph.py
+++ mycodo/sensors/atlas_ph.py
@@ -64,30 +64,22 @@ class AtlaspHSensor(AbstractSensor):
         """ Gets the sensor's pH measurement via UART/I2C """
         try:
             if ',' in self.sensor_sel.calibrate_sensor_measure:
-                # TODO: Remove this debug line
-                logger.info("pH sensor set to calibrate temperature")
+                logger.debug("pH sensor set to calibrate temperature")
 
                 device_id = self.sensor_sel.calibrate_sensor_measure.split(',')[0]
                 measurement = self.sensor_sel.calibrate_sensor_measure.split(',')[1]
                 last_measurement = read_last_influxdb(
                     device_id, measurement, duration_sec=300)
                 if last_measurement:
-
-                    # TODO: Remove this debug line
-                    logger.info("Latest temperature used to calibrate: {temp}".format(
+                    logger.debug("Latest temperature used to calibrate: {temp}".format(
                         temp=last_measurement[1]))
 
                     atlas_command = AtlasScientificCommand(self.sensor_sel)
-
-                    # TODO: Remove this debug line
-                    logger.info("Test 01")
-
                     ret_value, ret_msg = atlas_command.calibrate(
                         'temperature', temperature=last_measurement[1])
                     time.sleep(0.5)
 
-                    # TODO: Remove this debug line
-                    logger.info("Calibration returned: {val}, {msg}".format(
+                    logger.debug("Calibration returned: {val}, {msg}".format(
                         val=ret_value, msg=ret_msg))
 
             ph = None
