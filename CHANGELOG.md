80b760a88e487ab1f2fbe544d5c820222810b7f6
Kyle Gabriel <KyleTGabriel@gmail.com>
2017-10-23 23:18:26 -0400

    Prevent more than one active PID from using the same output (#108), Prevent a PID from using the same Raise and Lower output

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,8 @@ Because of a necessary database schema change, this update will deactivate all P
 
  - Fix "Too many files open" error when using the TSL2591 sensor ([#254](https://github.com/kizniche/mycodo/issues/254))
  - Fix bug that had the potential to lose data with certain graph display configurations
+ - Prevent more than one active PID from using the same output ([#108](https://github.com/kizniche/mycodo/issues/108))
+ - Prevent a PID from using the same Raise and Lower output
 
 ### Miscelaneous
 
--- mycodo/flaskutils.py
+++ mycodo/flaskutils.py
@@ -1339,67 +1339,73 @@ def pid_mod(form_mod_pid_base, form_mod_pid_pwm, form_mod_pid_relay):
     if not form_mod_pid_base.validate():
         flash_form_errors(form_mod_pid_base)
 
-    try:
-        sensor_unique_id = form_mod_pid_base.measurement.data.split(',')[0]
-        sensor = Sensor.query.filter(
-            Sensor.unique_id == sensor_unique_id).first()
-        if not sensor:
-            error.append(gettext(u"A valid sensor is required"))
-        if not error:
-            mod_pid = PID.query.filter(
-                PID.id == form_mod_pid_base.pid_id.data).first()
-            mod_pid.name = form_mod_pid_base.name.data
-            mod_pid.measurement = form_mod_pid_base.measurement.data
-            mod_pid.direction = form_mod_pid_base.direction.data
-            mod_pid.period = form_mod_pid_base.period.data
-            mod_pid.max_measure_age = form_mod_pid_base.max_measure_age.data
-            mod_pid.setpoint = form_mod_pid_base.setpoint.data
-            mod_pid.p = form_mod_pid_base.k_p.data
-            mod_pid.i = form_mod_pid_base.k_i.data
-            mod_pid.d = form_mod_pid_base.k_d.data
-            mod_pid.integrator_min = form_mod_pid_base.integrator_max.data
-            mod_pid.integrator_max = form_mod_pid_base.integrator_min.data
-            if form_mod_pid_base.method_id.data:
-                mod_pid.method_id = form_mod_pid_base.method_id.data
+    if (form_mod_pid_relay.raise_relay_id.data and
+            form_mod_pid_relay.raise_relay_id.data ==
+            form_mod_pid_relay.lower_relay_id.data):
+        error.append(gettext(u"Raise and lower outputs cannot be the same"))
+
+    sensor_unique_id = form_mod_pid_base.measurement.data.split(',')[0]
+    sensor = Sensor.query.filter(
+        Sensor.unique_id == sensor_unique_id).first()
+    if not sensor:
+        error.append(gettext(u"A valid sensor is required"))
+
+    mod_pid = PID.query.filter(
+        PID.id == form_mod_pid_base.pid_id.data).first()
+    mod_pid.name = form_mod_pid_base.name.data
+    mod_pid.measurement = form_mod_pid_base.measurement.data
+    mod_pid.direction = form_mod_pid_base.direction.data
+    mod_pid.period = form_mod_pid_base.period.data
+    mod_pid.max_measure_age = form_mod_pid_base.max_measure_age.data
+    mod_pid.setpoint = form_mod_pid_base.setpoint.data
+    mod_pid.p = form_mod_pid_base.k_p.data
+    mod_pid.i = form_mod_pid_base.k_i.data
+    mod_pid.d = form_mod_pid_base.k_d.data
+    mod_pid.integrator_min = form_mod_pid_base.integrator_max.data
+    mod_pid.integrator_max = form_mod_pid_base.integrator_min.data
+    if form_mod_pid_base.method_id.data:
+        mod_pid.method_id = form_mod_pid_base.method_id.data
+    else:
+        mod_pid.method_id = None
+
+    if mod_pid.pid_type == 'relay':
+        if not form_mod_pid_relay.validate():
+            flash_form_errors(form_mod_pid_relay)
+        else:
+            if form_mod_pid_relay.raise_relay_id.data:
+                mod_pid.raise_relay_id = form_mod_pid_relay.raise_relay_id.data
             else:
-                mod_pid.method_id = None
+                mod_pid.raise_relay_id = None
+            mod_pid.raise_min_duration = form_mod_pid_relay.raise_min_duration.data
+            mod_pid.raise_max_duration = form_mod_pid_relay.raise_max_duration.data
+            mod_pid.raise_min_off_duration = form_mod_pid_relay.raise_min_off_duration.data
+            if form_mod_pid_relay.lower_relay_id.data:
+                mod_pid.lower_relay_id = form_mod_pid_relay.lower_relay_id.data
+            else:
+                mod_pid.lower_relay_id = None
+            mod_pid.lower_min_duration = form_mod_pid_relay.lower_min_duration.data
+            mod_pid.lower_max_duration = form_mod_pid_relay.lower_max_duration.data
+            mod_pid.lower_min_off_duration = form_mod_pid_relay.lower_min_off_duration.data
 
-            if mod_pid.pid_type == 'relay':
-                if not form_mod_pid_relay.validate():
-                    flash_form_errors(form_mod_pid_relay)
-                else:
-                    if form_mod_pid_relay.raise_relay_id.data:
-                        mod_pid.raise_relay_id = form_mod_pid_relay.raise_relay_id.data
-                    else:
-                        mod_pid.raise_relay_id = None
-                    mod_pid.raise_min_duration = form_mod_pid_relay.raise_min_duration.data
-                    mod_pid.raise_max_duration = form_mod_pid_relay.raise_max_duration.data
-                    mod_pid.raise_min_off_duration = form_mod_pid_relay.raise_min_off_duration.data
-                    if form_mod_pid_relay.lower_relay_id.data:
-                        mod_pid.lower_relay_id = form_mod_pid_relay.lower_relay_id.data
-                    else:
-                        mod_pid.lower_relay_id = None
-                    mod_pid.lower_min_duration = form_mod_pid_relay.lower_min_duration.data
-                    mod_pid.lower_max_duration = form_mod_pid_relay.lower_max_duration.data
-                    mod_pid.lower_min_off_duration = form_mod_pid_relay.lower_min_off_duration.data
+    elif mod_pid.pid_type == 'pwm':
+        if not form_mod_pid_pwm.validate():
+            flash_form_errors(form_mod_pid_pwm)
+        else:
+            if form_mod_pid_relay.raise_relay_id.data:
+                mod_pid.raise_relay_id = form_mod_pid_pwm.raise_relay_id.data
+            else:
+                mod_pid.raise_relay_id = None
+            if form_mod_pid_relay.lower_relay_id.data:
+                mod_pid.lower_relay_id = form_mod_pid_pwm.lower_relay_id.data
+            else:
+                mod_pid.lower_relay_id = None
+            mod_pid.raise_min_duration = form_mod_pid_pwm.raise_min_duty_cycle.data
+            mod_pid.raise_max_duration = form_mod_pid_pwm.raise_max_duty_cycle.data
+            mod_pid.lower_min_duration = form_mod_pid_pwm.lower_min_duty_cycle.data
+            mod_pid.lower_max_duration = form_mod_pid_pwm.lower_max_duty_cycle.data
 
-            elif mod_pid.pid_type == 'pwm':
-                if not form_mod_pid_pwm.validate():
-                    flash_form_errors(form_mod_pid_pwm)
-                else:
-                    if form_mod_pid_relay.raise_relay_id.data:
-                        mod_pid.raise_relay_id = form_mod_pid_pwm.raise_relay_id.data
-                    else:
-                        mod_pid.raise_relay_id = None
-                    if form_mod_pid_relay.lower_relay_id.data:
-                        mod_pid.lower_relay_id = form_mod_pid_pwm.lower_relay_id.data
-                    else:
-                        mod_pid.lower_relay_id = None
-                    mod_pid.raise_min_duration = form_mod_pid_pwm.raise_min_duty_cycle.data
-                    mod_pid.raise_max_duration = form_mod_pid_pwm.raise_max_duty_cycle.data
-                    mod_pid.lower_min_duration = form_mod_pid_pwm.lower_min_duty_cycle.data
-                    mod_pid.lower_max_duration = form_mod_pid_pwm.lower_max_duty_cycle.data
-
+    try:
+        if not error:
             db.session.commit()
             # If the controller is active or paused, refresh variables in thread
             if mod_pid.is_activated:
@@ -1490,6 +1496,21 @@ def pid_activate(pid_id):
     # Check if associated sensor is activated
     pid = PID.query.filter(
         PID.id == pid_id).first()
+
+    pid_all = PID.query.filter(
+        PID.is_activated == True).all()
+
+    # Don't allow an output to be used with more than one active PID
+    for each_pid in pid_all:
+        if ((pid.raise_relay_id and
+                (pid.raise_relay_id == each_pid.raise_relay_id or
+                 pid.raise_relay_id == each_pid.lower_relay_id)) or
+            (pid.lower_relay_id and
+                (pid.lower_relay_id == each_pid.lower_relay_id or
+                 pid.lower_relay_id == each_pid.raise_relay_id))):
+            error.append(gettext(
+                u"Cannot activate PID controller if an output is already "
+                u"selected by another active PID controller"))
 
     sensor_unique_id = pid.measurement.split(',')[0]
     sensor = Sensor.query.filter(
