c5e1e9504ec63adc8a5e299ddf45e04f77facbdd
Kyle Gabriel <KyleTGabriel@gmail.com>
2017-12-25 13:59:17 -0500

    refix version number (whoops)

--- mycodo/config.py
+++ mycodo/config.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 import os
 from flask_babel import lazy_gettext
 
-MYCODO_VERSION = '4.5.0'
+MYCODO_VERSION = '5.5.0'
 ALEMBIC_VERSION = 'f0e4df767286'
 
 #  FORCE_UPGRADE_MASTER
