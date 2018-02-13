8d6f939ad980b67ee16bbe7711aefd51dcc725d3
Kyle Gabriel <KyleTGabriel@gmail.com>
2018-02-13 10:22:32 -0500

    Release v5.5.21

--- mycodo/config.py
+++ mycodo/config.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 import os
 from flask_babel import lazy_gettext
 
-MYCODO_VERSION = '5.5.20'
+MYCODO_VERSION = '5.5.21'
 ALEMBIC_VERSION = 'c08a535e4d49'
 
 #  FORCE_UPGRADE_MASTER
