06fcb8e9712fe36395c7319606d817fb89d07664
Kyle Gabriel <KyleTGabriel@gmail.com>
2018-03-24 20:20:59 -0400

    Release v5.6.9

--- mycodo/config.py
+++ mycodo/config.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 import os
 from flask_babel import lazy_gettext
 
-MYCODO_VERSION = '5.6.8'
+MYCODO_VERSION = '5.6.9'
 ALEMBIC_VERSION = '01ba9473fc96'
 
 #  FORCE_UPGRADE_MASTER
