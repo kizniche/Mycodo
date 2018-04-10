71710f1f657db0f5814a9a4132c5924fa7a7c9de
Kyle Gabriel <KyleTGabriel@gmail.com>
2018-04-10 00:09:26 -0400

    clear alembic versions

--- mycodo/config.py
+++ mycodo/config.py
@@ -10,7 +10,7 @@ import os
 from flask_babel import lazy_gettext
 
 MYCODO_VERSION = '6.0.0'
-ALEMBIC_VERSION = '758f88901bbc'
+ALEMBIC_VERSION = '032b48f920b8'
 
 #  FORCE_UPGRADE_MASTER
 #  Set True to enable upgrading to the master branch of the Mycodo repository.
