54a0bdb6db8f67b728aaa705b605a297becb7196
Kyle Gabriel <KyleTGabriel@gmail.com>
2018-04-13 16:02:32 -0400

    add missing help text

--- mycodo/scripts/mycodo_wrapper.c
+++ mycodo/scripts/mycodo_wrapper.c
@@ -140,6 +140,7 @@ int main(int argc, char *argv[]) {
 		printf("   restart:                    Restart the computer after a 10 second pause\n");
 		printf("   shutdown:                   Shutdown the computer after a 10 second pause\n");
 		printf("   upgrade:                    Upgrade Mycodo to the latest version on github\n");
+		printf("   upgrade-next-release:       Upgrade Mycodo to the next major version on github\n");
 		printf("   upgrade-master:             Upgrade Mycodo to the latest master branch on github\n");
 	}
 
