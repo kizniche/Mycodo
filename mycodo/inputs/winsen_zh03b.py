aaf7702aa3ad4be02de3e42a504b4f571823ecbb
Kyle Gabriel <KyleTGabriel@gmail.com>
2018-09-06 20:14:10 -0400

    Prevent variable from shadowing built-in 'bytes'

--- mycodo/inputs/winsen_zh03b.py
+++ mycodo/inputs/winsen_zh03b.py
@@ -163,11 +163,11 @@ class WINSEN_ZH03BSensor(AbstractInput):
         #    return ''.join( ["%c" % chr( int ( hexStr[i:i+2],16 ) ) \
         #                                   for i in range(0, len( hexStr ), 2) ] )
 
-        bytes = []
+        bytes_list = []
 
         hexStr = ''.join(hexStr.split(" "))
 
         for i in range(0, len(hexStr), 2):
-            bytes.append(chr(int(hexStr[i:i + 2], 16)))
+            bytes_list.append(chr(int(hexStr[i:i + 2], 16)))
 
-        return ''.join(bytes)
+        return ''.join(bytes_list)
