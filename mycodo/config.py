093818cab1ae1ece357ab48f721c3e0971351336
Kyle Gabriel <KyleTGabriel@gmail.com>
2018-10-10 12:36:32 -0400

    version number change

--- mycodo/config.py
+++ mycodo/config.py
@@ -8,7 +8,7 @@ from datetime import timedelta
 import os
 from flask_babel import lazy_gettext
 
-MYCODO_VERSION = '6.4.0'
+MYCODO_VERSION = '6.3.9'
 ALEMBIC_VERSION = '1421f1a02f25'
 
 #  FORCE_UPGRADE_MASTER
