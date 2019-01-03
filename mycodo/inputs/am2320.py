644102557e9266b7195b28595371960c2f3e7d15
Kyle Gabriel <KyleTGabriel@gmail.com>
2019-01-03 12:12:34 -0500

    Potential fix for AM2320 input module (#585)

--- mycodo/inputs/am2320.py
+++ mycodo/inputs/am2320.py
@@ -1,8 +1,6 @@
 # coding=utf-8
 import logging
-from time import sleep
-
-from struct import unpack
+import time
 
 from mycodo.databases.models import DeviceMeasurements
 from mycodo.inputs.base_input import AbstractInput
@@ -68,11 +66,13 @@ class InputModule(AbstractInput):
         self._measurements = None
         self.powered = False
         self.sensor = None
+        self.i2c_address = 0x5C
 
         if not testing:
-            from mycodo.mycodo_client import DaemonControl
+            from smbus2 import SMBus
             self.logger = logging.getLogger(
-                'mycodo.am2320_{id}'.format(id=input_dev.unique_id.split('-')[0]))
+                'mycodo.am2320_{id}'.format(
+                    id=input_dev.unique_id.split('-')[0]))
 
             self.device_measurements = db_retrieve_table_daemon(
                 DeviceMeasurements).filter(
@@ -80,21 +80,20 @@ class InputModule(AbstractInput):
 
             self.i2c_bus = input_dev.i2c_bus
             self.power_output_id = input_dev.power_output_id
-            self.control = DaemonControl()
             self.start_sensor()
-            self.sensor = AM2320(self.i2c_bus,logger=self.logger)
+            self.sensor = SMBus(self.i2c_bus)
 
     def get_measurement(self):
         """ Gets the humidity and temperature """
         return_dict = measurements_dict.copy()
 
-        self.sensor.read()
+        temperature, humidity = self.read()
 
         if self.is_enabled(0):
-            return_dict[0]['value'] = self.sensor.temperature
+            return_dict[0]['value'] = temperature
 
         if self.is_enabled(1):
-            return_dict[1]['value'] = self.sensor.humidity
+            return_dict[1]['value'] = humidity
 
         if (self.is_enabled(2) and
                 self.is_enabled(0) and
@@ -110,92 +109,24 @@ class InputModule(AbstractInput):
 
         return return_dict
 
-
-class AM2320(object):
-    """
-    AM2320 temperature and humidity sensor class.
-    
-    original source: https://github.com/8devices/IoTPy/
-
-    :param interface:  I2C interface id.
-    :type interface: :int
-    :param sensor_address: AM2320 sensor I2C address. Optional, default 0x5C (92).
-    :type sensor_address: int
-    """
-
-    I2C_ADDR_AM2320 = 0x5c  # 0xB8 >> 1
-    PARAM_AM2320_READ = 0x03
-    REG_AM2320_HUMIDITY_MSB = 0x00
-    REG_AM2320_HUMIDITY_LSB = 0x01
-    REG_AM2320_TEMPERATURE_MSB = 0x02
-    REG_AM2320_TEMPERATURE_LSB = 0x03
-    REG_AM2320_DEVICE_ID_BIT_24_31 = 0x0B
-
-    def __init__(self, interface, sensor_address=0x5c, logger=None):
-        from smbus2 import SMBus
-        self.logger = logger
-        self.interface = interface
-        self.address = sensor_address
-        self.temperature = -1000.0
-        self.humidity = -1
-        self.bus = SMBus(interface)
-
-    def _read_raw(self, command, regaddr, regcount):
-        try:
-            self.bus.write_i2c_block_data(
-                self.address, 0x00, [])
-            self.bus.write_i2c_block_data(
-                self.address, command, [regaddr, regcount])
-
-            sleep(0.002)
-
-            buf = self.bus.read_i2c_block_data(self.address, 0, 8)
-        except Exception:
-            self.logger.error("AM2320 Read error")
-            return None
-
-        buf_str = "".join(chr(x) for x in buf)
-
-        crc = unpack('<H', buf_str[-2:])[0]
-        if crc != self._am_crc16(buf[:-2]):
-            self.logger.error("AM2320 CRC error.")
-        return buf_str[2:-2]
-
-    def _am_crc16(self, buf):
-        crc = 0xFFFF
-        for c in buf:
-            crc ^= c
-            for i in range(8):
-                if crc & 0x01:
-                    crc >>= 1
-                    crc ^= 0xA001
-                else:
-                    crc >>= 1
-        return crc
-
-    def read_uid(self):
-        """
-        Read and return unique 32bit sensor ID.
-
-        :return: A unique 32bit sensor ID.
-        :rtype: int
-        """
-        resp = self._read_raw(
-            self.PARAM_AM2320_READ,
-            self.REG_AM2320_DEVICE_ID_BIT_24_31, 4)
-        uid = unpack('>I', resp)[0]
-        return uid
-
     def read(self):
-        """
-        Read and store temperature and humidity value.
+        try:
+            self.sensor.write_i2c_block_data(
+                self.i2c_address, 0x03, [0x00, 0x04])
+        except OSError as e:
+            self.logger.error(e)
+        time.sleep(0.02)
 
-        Read temperature and humidity registers from the sensor,
-        then convert and store them.
-        Use :func:`temperature` and :func:`humidity` to retrieve these values.
-        """
-        raw_data = self._read_raw(
-            self.PARAM_AM2320_READ,
-            self.REG_AM2320_HUMIDITY_MSB, 4)
-        self.temperature = unpack('>H', raw_data[-2:])[0] / 10.0
-        self.humidity = unpack('>H', raw_data[-4:2])[0] / 10.0
+        blocks = self.sensor.read_i2c_block_data(self.i2c_address, 0, 6)
+
+        humidity = ((blocks[2] << 8) + blocks[3]) / 10.0
+        temperature = ((blocks[4] << 8) + blocks[5]) / 10.0
+
+        return temperature, humidity
+
+    def setup(self):
+        try:
+            self.sensor.write_i2c_block_data(self.i2c_address, 0x00, [])
+        except OSError as e:
+            self.logger.error(e)
+        time.sleep(0.1)
