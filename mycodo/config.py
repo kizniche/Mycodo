4ec3e6e5d9c8fc1752e6d0c8563661562e03ddda
Kyle Gabriel <KyleTGabriel@gmail.com>
2019-01-23 14:31:03 -0500

    v7.1.3 Release

--- mycodo/config.py
+++ mycodo/config.py
@@ -12,7 +12,7 @@ from flask_babel import lazy_gettext
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from config_translations import TRANSLATIONS
 
-MYCODO_VERSION = '7.1.2'
+MYCODO_VERSION = '7.1.3'
 ALEMBIC_VERSION = '70c828e05255'
 
 #  FORCE_UPGRADE_MASTER
