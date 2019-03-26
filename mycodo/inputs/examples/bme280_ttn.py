a2199fc43453783f59a8198106c982de0561debf
Kyle Gabriel <KyleTGabriel@gmail.com>
2019-03-26 00:26:32 -0400

    add example modules

---
+++ mycodo/inputs/examples/bme280_ttn.py
@@ -0,0 +1,211 @@
+# coding=utf-8
+#
+# Example module for measuring with the BME280 and sending
+# the measurement to a serial device. For accompaniment with
+# the The Things Network (TTN) Data Storage Input module
+#
+# Use this module to send measurements via serial to a
+# LoRaWAN-enabled device, which transmits the data to TTN.
+#
+# Comment will be updated with other code to go along with this module
+#
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+# Based on the BMP280 driver with BME280 changes provided by
+# David J Taylor, Edinburgh (www.satsignal.eu)
+import logging
+import os
+import time
+from flask_babel import lazy_gettext
+
+from mycodo.databases.models import DeviceMeasurements
+from mycodo.inputs.base_input import AbstractInput
+from mycodo.inputs.sensorutils import calculate_altitude
+from mycodo.inputs.sensorutils import calculate_dewpoint
+from mycodo.inputs.sensorutils import calculate_vapor_pressure_deficit
+from mycodo.utils.database import db_retrieve_table_daemon
+
+# Measurements
+measurements_dict = {
+    0: {
+        'measurement': 'temperature',
+        'unit': 'C'
+    },
+    1: {
+        'measurement': 'humidity',
+        'unit': 'percent'
+    },
+    2: {
+        'measurement': 'pressure',
+        'unit': 'Pa'
+    },
+    3: {
+        'measurement': 'dewpoint',
+        'unit': 'C'
+    },
+    4: {
+        'measurement': 'altitude',
+        'unit': 'm'
+    },
+    5: {
+        'measurement': 'vapor_pressure_deficit',
+        'unit': 'Pa'
+    }
+
+}
+
+# Input information
+INPUT_INFORMATION = {
+    'input_name_unique': 'BME280_TTN',
+    'input_manufacturer': 'BOSCH',
+    'input_name': 'BME280 (->Serial->TTN)',
+    'measurements_name': 'Pressure/Humidity/Temperature',
+    'measurements_dict': measurements_dict,
+
+    'options_enabled': [
+        'i2c_location',
+        'custom_options',
+        'measurements_select',
+        'period',
+        'pre_output'
+    ],
+    'options_disabled': ['interface'],
+
+    'dependencies_module': [
+        ('pip-pypi', 'locket', 'locket'),
+        ('pip-pypi', 'serial', 'pyserial'),
+        ('pip-pypi', 'Adafruit_GPIO', 'Adafruit_GPIO'),
+        ('pip-git', 'Adafruit_BME280', 'git://github.com/adafruit/Adafruit_Python_BME280.git#egg=adafruit-bme280')
+    ],
+
+    'interfaces': ['I2C'],
+    'i2c_location': [
+        '0x76',
+        '0x77'
+    ],
+    'i2c_address_editable': False,
+
+    'custom_options': [
+        {
+            'id': 'serial_device',
+            'type': 'text',
+            'default_value': '/dev/ttyUSB0',
+            'name': lazy_gettext('Serial Device'),
+            'phrase': lazy_gettext('The serial device to write to')
+        }
+    ]
+}
+
+
+class InputModule(AbstractInput):
+    """
+    A sensor support class that measures the BME280's humidity, temperature,
+    and pressure, them calculates the altitude and dew point
+
+    """
+
+    def __init__(self, input_dev, testing=False):
+        super(InputModule, self).__init__()
+        self.logger = logging.getLogger("mycodo.inputs.bme280_ttn")
+        self.timer = 0
+
+        if not testing:
+            from Adafruit_BME280 import BME280
+            import serial
+            import locket
+            self.logger = logging.getLogger(
+                "mycodo.bme280_ttn_{id}".format(id=input_dev.unique_id.split('-')[0]))
+
+            self.device_measurements = db_retrieve_table_daemon(
+                DeviceMeasurements).filter(
+                    DeviceMeasurements.device_id == input_dev.unique_id)
+
+            if input_dev.custom_options:
+                for each_option in input_dev.custom_options.split(';'):
+                    option = each_option.split(',')[0]
+                    value = each_option.split(',')[1]
+                    if option == 'serial_device':
+                        self.serial_device = value
+
+            self.i2c_address = int(str(input_dev.i2c_location), 16)
+            self.i2c_bus = input_dev.i2c_bus
+            self.sensor = BME280(address=self.i2c_address,
+                                 busnum=self.i2c_bus)
+            self.serial = serial
+            self.serial_send = None
+            self.locket = locket
+            self.lock = None
+            self.lock_file = "/var/lock/mycodo_ttn.lock"
+            self.locked = False
+            self.ttn_serial_error = False
+
+    def lock_setup(self):
+        self.lock = self.locket.lock_file(self.lock_file, timeout=10)
+        try:
+            self.lock.acquire()
+            self.locked = True
+        except self.locket.LockError:
+            self.logger.error("Could not acquire input lock. Breaking for future locking.")
+            try:
+                os.remove(self.lock_file)
+            except OSError:
+                self.logger.error("Can't delete lock file: Lock file doesn't exist.")
+
+    def lock_release(self):
+        try:
+            if self.locked:
+                self.lock.release()
+                self.locked = False
+        except AttributeError:
+            self.logger.error("Can't release lock: Lock file not present.")
+
+    def get_measurement(self):
+        """ Gets the measurement in units by reading the """
+        return_dict = measurements_dict.copy()
+
+        if self.is_enabled(0):
+            return_dict[0]['value'] = self.sensor.read_temperature()
+
+        if self.is_enabled(1):
+            return_dict[1]['value'] = self.sensor.read_humidity()
+
+        if self.is_enabled(2):
+            return_dict[2]['value'] = self.sensor.read_pressure()
+
+        if (self.is_enabled(3) and
+                self.is_enabled(0) and
+                self.is_enabled(1)):
+            return_dict[3]['value'] = calculate_dewpoint(
+                return_dict[0]['value'], return_dict[1]['value'])
+
+        if self.is_enabled(4) and self.is_enabled(2):
+            return_dict[4]['value'] = calculate_altitude(return_dict[2]['value'])
+
+        if (self.is_enabled(5) and
+                self.is_enabled(0) and
+                self.is_enabled(1)):
+            return_dict[5]['value'] = calculate_vapor_pressure_deficit(
+                return_dict[0]['value'], return_dict[1]['value'])
+
+        try:
+            now = time.time()
+            if now > self.timer:
+                self.timer = now + 80
+                # "B" designates this data belonging to the BME280
+                string_send = 'B,{},{},{}'.format(
+                    return_dict[1]['value'],
+                    return_dict[2]['value'],
+                    return_dict[0]['value'])
+                self.lock_setup()
+                self.serial_send = self.serial.Serial(self.serial_device, 9600)
+                self.serial_send.write(string_send.encode())
+                time.sleep(4)
+                self.lock_release()
+                self.ttn_serial_error = False
+        except:
+            if not self.ttn_serial_error:
+                # Only send this error once if it continually occurs
+                self.logger.error("TTN: Could not send serial")
+                self.ttn_serial_error = True
+
+        return return_dict
---
+++ mycodo/inputs/examples/k30_ttn.py
@@ -0,0 +1,179 @@
+# coding=utf-8
+#
+# Example module for measuring with the K30 and sending
+# the measurement to a serial device. For accompaniment with
+# the The Things Network (TTN) Data Storage Input module
+#
+# Use this module to send measurements via serial to a
+# LoRaWAN-enabled device, which transmits the data to TTN.
+#
+# Comment will be updated with other code to go along with this module
+#
+import logging
+import os
+import time
+from flask_babel import lazy_gettext
+
+from mycodo.inputs.base_input import AbstractInput
+from mycodo.inputs.sensorutils import is_device
+
+airtime_seconds = 0.0515  # 51.5 ms
+ttn_max_seconds_transmit_per_day = 30
+max_transmissions_per_day = ttn_max_seconds_transmit_per_day / airtime_seconds
+min_seconds_between_transmissions = 86400 / max_transmissions_per_day
+
+# Measurements
+measurements_dict = {
+    0: {
+        'measurement': 'co2',
+        'unit': 'ppm'
+    }
+}
+
+# Input information
+INPUT_INFORMATION = {
+    'input_name_unique': 'K30_TTN',
+    'input_manufacturer': 'CO2Meter',
+    'input_name': 'K30 (->Serial->TTN)',
+    'measurements_name': 'CO2',
+    'measurements_dict': measurements_dict,
+
+    'options_enabled': [
+        'uart_location',
+        'uart_baud_rate',
+        'custom_options',
+        'period',
+        'pre_output'
+    ],
+    'options_disabled': ['interface'],
+
+    'dependencies_module': [
+        ('pip-pypi', 'locket', 'locket'),
+        ('pip-pypi', 'serial', 'pyserial')
+    ],
+
+    'interfaces': ['UART'],
+    'uart_location': '/dev/ttyAMA0',
+    'uart_baud_rate': 9600,
+
+    'custom_options': [
+        {
+            'id': 'serial_device',
+            'type': 'text',
+            'default_value': '/dev/ttyUSB0',
+            'name': lazy_gettext('Serial Device'),
+            'phrase': lazy_gettext('The serial device to write to')
+        }
+    ]
+}
+
+
+class InputModule(AbstractInput):
+    """ A sensor support class that monitors the K30's CO2 concentration """
+
+    def __init__(self, input_dev, testing=False):
+        super(InputModule, self).__init__()
+        self.logger = logging.getLogger("mycodo.inputs.k30_ttn")
+        self.timer = 0
+
+        if not testing:
+            import serial
+            import locket
+            self.logger = logging.getLogger(
+                "mycodo.k30_ttn_{id}".format(id=input_dev.unique_id.split('-')[0]))
+
+            if input_dev.custom_options:
+                for each_option in input_dev.custom_options.split(';'):
+                    option = each_option.split(',')[0]
+                    value = each_option.split(',')[1]
+                    if option == 'serial_device':
+                        self.serial_device = value
+
+            self.uart_location = input_dev.uart_location
+            self.baud_rate = input_dev.baud_rate
+            # Check if device is valid
+            self.uart_location = is_device(self.uart_location)
+            if self.uart_location:
+                try:
+                    self.ser = serial.Serial(self.uart_location,
+                                             baudrate=self.baud_rate,
+                                             timeout=1)
+                except serial.SerialException:
+                    self.logger.exception('Opening serial')
+            else:
+                self.logger.error(
+                    'Could not open "{dev}". '
+                    'Check the device location is correct.'.format(
+                        dev=self.uart_location))
+
+            self.serial = serial
+            self.serial_send = None
+            self.locket = locket
+            self.lock = None
+            self.lock_file = "/var/lock/mycodo_ttn.lock"
+            self.locked = False
+            self.ttn_serial_error = False
+            self.logger.debug(
+                "Min time between transmissions: {} seconds".format(
+                    min_seconds_between_transmissions))
+
+    def lock_setup(self):
+        self.lock = self.locket.lock_file(self.lock_file, timeout=10)
+        try:
+            self.lock.acquire()
+            self.locked = True
+        except self.locket.LockError:
+            self.logger.error("Could not acquire input lock. Breaking for future locking.")
+            try:
+                os.remove(self.lock_file)
+            except OSError:
+                self.logger.error("Can't delete lock file: Lock file doesn't exist.")
+
+    def lock_release(self):
+        try:
+            if self.locked:
+                self.lock.release()
+                self.locked = False
+        except AttributeError:
+            self.logger.error("Can't release lock: Lock file not present.")
+
+    def get_measurement(self):
+        """ Gets the K30's CO2 concentration in ppmv via UART"""
+        if not self.uart_location:  # Don't measure if device isn't validated
+            return None
+
+        return_dict = measurements_dict.copy()
+
+        co2 = None
+
+        self.ser.flushInput()
+        time.sleep(1)
+        self.ser.write(bytearray([0xfe, 0x44, 0x00, 0x08, 0x02, 0x9f, 0x25]))
+        time.sleep(.01)
+        resp = self.ser.read(7)
+        if len(resp) != 0:
+            high = resp[3]
+            low = resp[4]
+            co2 = (high * 256) + low
+
+        return_dict[0]['value'] = co2
+
+        try:
+            now = time.time()
+            if now > self.timer:
+                self.timer = now + min_seconds_between_transmissions
+                # "K" designates this data belonging to the K30
+                string_send = 'K,{}'.format(return_dict[0]['value'])
+                self.lock_setup()
+                self.serial_send = self.serial.Serial(self.serial_device, 9600)
+                self.serial_send.write(string_send.encode())
+                time.sleep(4)
+                self.lock_release()
+                self.ttn_serial_error = False
+        except:
+            if not self.ttn_serial_error:
+                # Only send this error once if it continually occurs
+                self.logger.error("TTN: Could not send serial")
+                self.ttn_serial_error = True
+
+        return return_dict
