50726ba40d0c171d04c2ccb9c1c8a5777ef4d21a
Kyle Gabriel <kyletgabriel@gmail.com>
2019-06-07 10:09:20 -0400

    fix pytest

--- mycodo/tests/software_tests/test_inputs/test_abstract_input_class.py
+++ mycodo/tests/software_tests/test_inputs/test_abstract_input_class.py
@@ -13,7 +13,7 @@ def test_abstract_input_get_measurement_method_logs_when_not_implemented():
     """  verify that methods that are not overwritten log as errors"""
     with LogCapture() as log_cap:
         with pytest.raises(NotImplementedError):
-            AbstractInput().get_measurement()
+            AbstractInput(None).get_measurement()
     expected_error = ('mycodo.inputs.base_input', 'ERROR', ('AbstractInput did not overwrite the get_measurement() '
                                                             'method. All subclasses of the AbstractInput '
                                                             'class are required to overwrite this method'))
