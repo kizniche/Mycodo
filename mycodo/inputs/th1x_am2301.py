33c5812e4c79ddc935c092894a2f8c9ef9de9d22
Kyle Gabriel <kizniche@users.noreply.github.com>
2019-07-12 19:51:44 -0400

    Remove debug line

--- mycodo/inputs/th1x_am2301.py
+++ mycodo/inputs/th1x_am2301.py
@@ -4,7 +4,7 @@
 # https://github.com/arendst/Sonoff-Tasmota
 import datetime
 import json
-import traceback
+
 import requests
 from flask_babel import lazy_gettext
 
@@ -124,4 +124,3 @@ class InputModule(AbstractInput):
             return self.return_dict
         except Exception:
             self.logger.exception("TEST01")
-            self.logger.error("TEST02: {}".format(traceback.format_exc()))
