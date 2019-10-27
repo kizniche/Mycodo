abb3ce6e5a35d61a317923558ea4dc9fff6fb63e
Kyle Gabriel <KyleTGabriel@gmail.com>
2019-10-26 23:49:20 -0400

    Add specific Input SQL entry query to API (#705)

--- mycodo/mycodo_flask/api.py
+++ mycodo/mycodo_flask/api.py
@@ -31,19 +31,38 @@ api = Api(api_bp, version='1.0', title='Mycodo API',
 ns_settings = api.namespace('settings', description='Settings operations')
 
 
-@ns_settings.route('/inputs')
-class Users(Resource):
+@ns_settings.route('/input/<uuid>')
+@ns_settings.doc(params={'uuid': 'The Unique ID'})
+class InputSingle(Resource):
     """Interacts with Input settings in the SQL database"""
-    @api.doc(responses={
+    @ns_settings.doc(responses={
         200: 'Success',
         401: 'Invalid API Key',
-        403: 'Invalid Permissions'
+        403: 'Insufficient Permissions'
     })
-    @ns_settings.doc('dump_users')
-    @api.doc(security='apikey')
+    @ns_settings.doc(security='apikey')
+    @flask_login.login_required
+    def get(self, uuid):
+        """Shows the Input settings for an input with the specified unique_id"""
+        if not utils_general.user_has_permission('view_settings'):
+            return 'You do not have permission to access this.', 401
+        input_schema = InputSchema()
+        input_ = Input.query.filter_by(unique_id=uuid).first()
+        return input_schema.dump(input_)
+
+
+@ns_settings.route('/inputs')
+class InputDump(Resource):
+    """Interacts with Input settings in the SQL database"""
+    @ns_settings.doc(responses={
+        200: 'Success',
+        401: 'Invalid API Key',
+        403: 'Insufficient Permissions'
+    })
+    @ns_settings.doc(security='apikey')
     @flask_login.login_required
     def get(self):
-        """Dumps all Input settings"""
+        """Shows all Input settings"""
         if not utils_general.user_has_permission('view_settings'):
             return 'You do not have permission to access this.', 401
         input_schema = InputSchema()
@@ -51,18 +70,17 @@ class Users(Resource):
 
 
 @ns_settings.route('/users')
-class Users(Resource):
+class UserDump(Resource):
     """Interacts with User settings in the SQL database"""
-    @api.doc(responses={
+    @ns_settings.doc(responses={
         200: 'Success',
         401: 'Invalid API Key',
-        403: 'Invalid Permissions'
+        403: 'Insufficient Permissions'
     })
-    @ns_settings.doc('dump_users')
-    @api.doc(security='apikey')
+    @ns_settings.doc(security='apikey')
     @flask_login.login_required
     def get(self):
-        """Dumps all User settings"""
+        """Shows all User settings"""
         if not utils_general.user_has_permission('view_settings'):
             return 'You do not have permission to access this.', 401
         user_schema = UserSchema()
