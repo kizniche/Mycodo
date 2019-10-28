63b6e3d459f19dfe44626ce05a446a7277eca2ec
Kyle Gabriel <kyletgabriel@gmail.com>
2019-10-28 14:41:31 -0400

    api manual test

--- mycodo/scripts/generate_manual_api.sh
+++ mycodo/scripts/generate_manual_api.sh
@@ -10,4 +10,5 @@ cd ${INSTALL_DIRECTORY}
 
 wget --no-check-certificate https://192.168.0.9/api/swagger.json ${INSTALL_DIRECTORY}/swagger.json
 npx redoc-cli bundle -o ${INSTALL_DIRECTORY}/mycodo-api.html ${INSTALL_DIRECTORY}/swagger.xml
-cp ${INSTALL_DIRECTORY}/mycodo-api.html ${INSTALL_DIRECTORY}/mycodo/mycodo_flask/templates/manual-api.html
+cp ${INSTALL_DIRECTORY}/mycodo-api.html ${INSTALL_DIRECTORY}/mycodo/mycodo_flask/templates/mycodo-api.html
+cp ${INSTALL_DIRECTORY}/mycodo-api.html ${INSTALL_DIRECTORY}/docs/mycodo-api.html
