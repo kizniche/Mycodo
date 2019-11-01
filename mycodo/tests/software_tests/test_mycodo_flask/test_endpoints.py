2565c4d331212e50e3c18d863070a7f1faf81815
Kyle Gabriel <kyletgabriel@gmail.com>
2019-11-01 18:54:42 -0400

    fix API tests

--- mycodo/tests/software_tests/test_mycodo_flask/test_endpoints.py
+++ mycodo/tests/software_tests/test_mycodo_flask/test_endpoints.py
@@ -110,11 +110,18 @@ def test_api_when_not_logged_in(testapp):
     """
     print("\nTest: test_api_when_not_logged_in")
     routes = [
-        'inputs/',
-        'outputs/',
-        'measurements/',
-        'pids/',
-        'users/'
+        'choices/controllers',
+        'choices/inputs/measurements',
+        'choices/output/devices',
+        'choices/output/measurements',
+        'choices/pid/measurements',
+        'settings/device_measurements/',
+        'settings/inputs/',
+        'settings/measurements/',
+        'settings/outputs/',
+        'settings/pids/',
+        'settings/units/',
+        'settings/users/'
     ]
     for route in routes:
         returns_401_unauthorized(testapp=testapp, endpoint='/api/{add}'.format(add=route))
