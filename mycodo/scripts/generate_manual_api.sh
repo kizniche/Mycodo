9c6b6aeca75c5d0cf296f066f0b5a3e16978fcee
Kyle Gabriel <kyletgabriel@gmail.com>
2019-11-10 17:32:05 -0500

    tweak API documentation links

--- mycodo/scripts/generate_manual_api.sh
+++ mycodo/scripts/generate_manual_api.sh
@@ -9,6 +9,13 @@
 INSTALL_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd -P )
 cd "${INSTALL_DIRECTORY}" || return
 
+# check if pandoc is installed
+if [[ $(command -v pandoc) ]]
+then
+  # Generate HTML (HTML5) file
+  pandoc --table-of-contents -H "${INSTALL_DIRECTORY}"/mycodo/scripts/pandoc.css_style --self-contained -s -S -t html5 -o "${INSTALL_DIRECTORY}"/mycodo/mycodo_flask/static/manual/mycodo-api-info.html "${INSTALL_DIRECTORY}"/mycodo-api.rst
+fi
+
 if curl -k -s --head --request GET https://192.168.0.9/api/swagger.json | grep "200 OK" > /dev/null; then
   if [[ $(command -v redoc-cli) ]]
   then
