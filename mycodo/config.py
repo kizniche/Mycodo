1c0bad0aad92ad746fa0f1030841cda7e7592b94
Kyle Gabriel <kyletgabriel@gmail.com>
2020-07-25 19:38:46 -0400

    v8.6.4 Bugfix Release

--- mycodo/config.py
+++ mycodo/config.py
@@ -13,7 +13,7 @@ from flask_babel import lazy_gettext
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from config_translations import TRANSLATIONS
 
-MYCODO_VERSION = '8.6.3'
+MYCODO_VERSION = '8.6.4'
 ALEMBIC_VERSION = '4d3258ef5864'
 
 #  FORCE_UPGRADE_MASTER
