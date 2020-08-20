88327c1a93404006dba6e48d0a0baced7914d671
Kyle Gabriel <KyleTGabriel@gmail.com>
2020-08-20 19:33:04 -0400

    Fix inability to generate Widget HTML (#817)

--- mycodo/widgets/widget_graph_synchronous.py
+++ mycodo/widgets/widget_graph_synchronous.py
@@ -506,7 +506,7 @@ WIDGET_INFORMATION = {
                 chart[chart_number].series[series].addPoint({
                     x: time_point,
                     title: data[i][1],
-                    text: data[i][2].replace(/(?:\\r\\n|\\r|\\n)/g, '<br>').replace(/  /g, '░░')
+                    text: data[i][2].replace(/(?:\\r\\n|\\r|\\n)/g, '<br>').replace(/  /g, '\\u2591\\u2591')
                 }, false, graph_shift);
                 note_timestamps.push(time_point);
               }
