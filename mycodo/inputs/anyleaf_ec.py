ddbd72f2d0f40bb3c7241c6db022c67bee4643f2
David O'Connor <david.alan.oconnor@gmail.com>
2021-05-06 22:41:57 -0400

    Undid a previous reversion

--- mycodo/inputs/anyleaf_ec.py
+++ mycodo/inputs/anyleaf_ec.py
@@ -33,16 +33,15 @@ INPUT_INFORMATION = {
         'uart_location',
         'period',
     ],
-    'options_disabled': [],
 
     'dependencies_module': [
-        # These aren't explicitly required for ec measurements,
-        # but are required by the multi-sensor`AnyLeaf` module.
-        ('apt', 'python3-numpy', 'python3-numpy'),
+        ('apt', 'libjpeg-dev', 'libjpeg-dev'),
+        ('apt', 'zlib1g-dev', 'zlib1g-dev'),
+        ('pip-pypi', 'PIL', 'Pillow==8.1.2'),
         ('apt', 'python3-scipy', 'python3-scipy'),
         ('pip-pypi', 'usb.core', 'pyusb==1.1.1'),
         ('pip-pypi', 'adafruit_extended_bus', 'Adafruit-extended-bus==1.0.1'),
-        ('pip-pypi', 'anyleaf', 'anyleaf')
+        ('pip-pypi', 'anyleaf', 'anyleaf==0.1.8.1')
     ],
 
     'interfaces': ['UART'],
@@ -50,21 +49,6 @@ INPUT_INFORMATION = {
     'uart_baud_rate': 9600,
 
     'custom_options': [
-        # todo: Look at this once you implement calibration.
-        # {
-        #     'id': 'cal_v',
-        #     'type': 'float',
-        #     'default_value': 0.4,
-        #     'name': "{}: {} ({})".format(lazy_gettext('Calibrate'), lazy_gettext('Voltage'), lazy_gettext('Internal')),
-        #     'phrase': 'Calibration data: internal voltage'
-        # },
-        # {
-        #     'id': 'cal_orp',
-        #     'type': 'float',
-        #     'default_value': 400.0,
-        #     'name': "{}: {} ({})".format(lazy_gettext('Calibrate'), lazy_gettext('ORP'), lazy_gettext('Internal')),
-        #     'phrase': 'Calibration data: internal ORP'
-        # },
         {
             'id': 'K',
             'type': 'float',
@@ -72,26 +56,6 @@ INPUT_INFORMATION = {
             'name': "{}".format(lazy_gettext('K')),  # todo: What should this be?
             'phrase': 'Conductivity constant K',
         }
-        # {
-        #     'id': 'temperature_comp_meas',
-        #     'type': 'select_measurement',
-        #     'default_value': '',
-        #     'options_select': [
-        #         'Input',
-        #         'Math'
-        #     ],
-        #     'name': "{}: {}".format(lazy_gettext('Temperature Compensation'), lazy_gettext('Measurement')),
-        #     'phrase': lazy_gettext('Select a measurement for temperature compensation')
-        # },
-        # {
-        #     'id': 'max_age',
-        #     'type': 'integer',
-        #     'default_value': 120,
-        #     'required': True,
-        #     'constraints_pass': constraints_pass_positive_value,
-        #     'name': "{}: {}".format(lazy_gettext('Temperature Compensation'), lazy_gettext('Max Age')),
-        #     'phrase': lazy_gettext('The maximum age (seconds) of the measurement to use for temperature compensation')
-        # }
     ],
     'custom_actions_message': """""",
     'custom_actions': [
@@ -119,24 +83,6 @@ class InputModule(AbstractInput):
             k = 1.0
 
         self.sensor = EcSensor(K=k)
-
-
-    # todo: Implement calibration procedure here.
-    # def calibrate(self, args_dict):
-    #     """ Auto-calibrate """
-    #     if 'calibration_orp' not in args_dict:
-    #         self.logger.error("Cannot conduct calibration without a buffer ORP value")
-    #         return
-    #     if not isinstance(args_dict['calibration_orp'], float) and not isinstance(args_dict['calibration_orp'], int):
-    #         self.logger.error("buffer value does not represent a number: '{}', type: {}".format(
-    #             args_dict['calibration_orp'], type(args_dict['calibration_orp'])))
-    #         return
-    #
-    #     v = self.sensor.calibrate(args_dict['calibration_orp'])  # For this session
-    #
-    #     # For future sessions
-    #     self.set_custom_option("cal_orp", args_dict['calibration_orp'])
-    #     self.set_custom_option("cal_v", v)
 
     def get_measurement(self):
         """ Gets the measurement """
