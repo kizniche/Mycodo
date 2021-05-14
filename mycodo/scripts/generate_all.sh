a7c4eaeab7496658b3b4fe3dac06de85e75b7cf1
Kyle Gabriel <kizniche@users.noreply.github.com>
2021-05-13 22:47:51 -0400

    revert travisci changes

--- mycodo/scripts/generate_all.sh
+++ mycodo/scripts/generate_all.sh
@@ -20,5 +20,5 @@ INSTALL_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd -P )
 "${INSTALL_DIRECTORY}"/env/bin/python "${INSTALL_DIRECTORY}"/mycodo/scripts/generate_manual_outputs.py
 "${INSTALL_DIRECTORY}"/env/bin/python "${INSTALL_DIRECTORY}"/mycodo/scripts/generate_manual_functions.py
 "${INSTALL_DIRECTORY}"/env/bin/python "${INSTALL_DIRECTORY}"/mycodo/scripts/generate_manual_widgets.py
-/bin/bash "${INSTALL_DIRECTORY}"/mycodo/scripts/generate_manual_api.sh "${1}"
+/bin/bash "${INSTALL_DIRECTORY}"/mycodo/scripts/generate_manual_api.sh
 /bin/bash "${INSTALL_DIRECTORY}"/mycodo/scripts/generate_translations_pybabel.sh
--- mycodo/scripts/generate_manual_api.sh
+++ mycodo/scripts/generate_manual_api.sh
@@ -7,7 +7,7 @@
 # sudo npm install -g npx
 #
 
-API_SERV_IP="${1}"
+API_SERV_IP="192.168.0.9"
 
 INSTALL_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd -P )
 
