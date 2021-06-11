5afafe9a057b76d8b3bb7c916a7ab48e42a7543a
Kyle Gabriel <kyletgabriel@gmail.com>
2021-06-11 10:17:00 -0400

    update translations

--- mycodo/scripts/generate_translations_pybabel.sh
+++ mycodo/scripts/generate_translations_pybabel.sh
@@ -9,9 +9,16 @@
 #
 
 INSTALL_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd -P )
+CURRENT_VERSION=$("${INSTALL_DIRECTORY}"/env/bin/python3 "${INSTALL_DIRECTORY}"/mycodo/utils/github_release_info.py -c 2>&1)
+
+INFO_ARGS=(
+  --project "Mycodo"
+  --version "${CURRENT_VERSION}"
+  --copyright "Kyle T. Gabriel"
+  --msgid-bugs-address "mycodo@kylegabriel.com"
+)
 
 cd "${INSTALL_DIRECTORY}"/mycodo || return
 
-"${INSTALL_DIRECTORY}"/env/bin/pybabel extract -F babel.cfg -k lazy_gettext -o messages.pot .
-"${INSTALL_DIRECTORY}"/env/bin/pybabel update -i messages.pot -d mycodo_flask/translations
-rm -f "${INSTALL_DIRECTORY}"/mycodo/messages.pot
+"${INSTALL_DIRECTORY}"/env/bin/pybabel extract "${INFO_ARGS[@]}" -s -F babel.cfg -k lazy_gettext -o mycodo_flask/translations/messages.pot .
+"${INSTALL_DIRECTORY}"/env/bin/pybabel update --update-header-comment -i mycodo_flask/translations/messages.pot -d mycodo_flask/translations
