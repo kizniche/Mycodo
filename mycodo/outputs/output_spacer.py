d48757fdb807e37e63d5b8acab64c9e4f9164544
Kyle Gabriel <kyletgabriel@gmail.com>
2021-11-29 17:49:47 -0500

    remove unused library key in spacer output module

--- mycodo/outputs/output_spacer.py
+++ mycodo/outputs/output_spacer.py
@@ -4,7 +4,6 @@ from flask_babel import lazy_gettext
 OUTPUT_INFORMATION = {
     'output_name_unique': 'output_spacer',
     'output_name': lazy_gettext('Spacer'),
-    'output_library': '',
     'measurements_dict': {},
     'channels_dict': {},
     'output_types': [],
