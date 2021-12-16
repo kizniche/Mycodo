879e1a4b53f79def0ce9fb885e10844d10c41e16
Kyle Gabriel <kyletgabriel@gmail.com>
2021-12-16 18:29:15 -0500

    Fix SCD4x Input

--- mycodo/inputs/scd4x_circuitpython.py
+++ mycodo/inputs/scd4x_circuitpython.py
@@ -123,6 +123,8 @@ class InputModule(AbstractInput):
         self.persist_settings = None
 
         if not testing:
+            self.setup_custom_options(
+                INPUT_INFORMATION['custom_options'], input_dev)
             self.initialize_input()
 
     def initialize_input(self):
