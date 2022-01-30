23c01d7096e31062dd0a1d53ffa1ed92c01d7234
Chris Coulson <ccoulson@gmail.com>
2022-01-29 22:05:54 -0500

    Update scd4x_circuitpython.py

--- mycodo/inputs/scd4x_circuitpython.py
+++ mycodo/inputs/scd4x_circuitpython.py
@@ -51,7 +51,7 @@ INPUT_INFORMATION = {
     'dependencies_module': [
         ('pip-pypi', 'usb.core', 'pyusb==1.1.1'),
         ('pip-pypi', 'adafruit_extended_bus', 'Adafruit-extended-bus==1.0.2'),
-        ('pip-pypi', 'adafruit_scd4x', 'adafruit-circuitpython-scd4x==3.4.1')
+        ('pip-pypi', 'adafruit_scd4x', 'adafruit-circuitpython-scd4x==1.2.2')
     ],
 
     'interfaces': ['I2C'],
