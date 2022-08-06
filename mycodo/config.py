1cc147472638677648c6b5e79c0c2a6194e98f4d
Kyle Gabriel <kyletgabriel@gmail.com>
2022-08-06 17:27:01 -0400

    update alembic version

--- mycodo/config.py
+++ mycodo/config.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from config_translations import TRANSLATIONS as T
 
 MYCODO_VERSION = '8.13.10'
-ALEMBIC_VERSION = '2c70a9e9e131'
+ALEMBIC_VERSION = '07c7c8ebc195'
 
 # FORCE UPGRADE MASTER
 # Set True to enable upgrading to the master branch of the Mycodo repository.
