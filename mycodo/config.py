3252f7c3e5e2d55aebc0d9d86155266e1dd4540d
Kyle Gabriel <kyletgabriel@gmail.com>
2022-08-20 16:19:56 -0400

    v8.14.2 release

--- mycodo/config.py
+++ mycodo/config.py
@@ -13,7 +13,7 @@ from flask_babel import lazy_gettext as lg
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from config_translations import TRANSLATIONS as T
 
-MYCODO_VERSION = '8.14.1'
+MYCODO_VERSION = '8.14.2'
 ALEMBIC_VERSION = '07c7c8ebc195'
 
 # FORCE UPGRADE MASTER
