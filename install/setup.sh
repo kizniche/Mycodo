6e8ee833ad124757675dce3ff0e5cd62d4d08229
Kyle Gabriel <kyletgabriel@gmail.com>
2023-08-20 11:09:35 -0400

    fix influxdb selection during install

--- install/setup.sh
+++ install/setup.sh
@@ -8,6 +8,10 @@
 INSTALL_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd -P )
 INSTALL_CMD="/bin/bash ${INSTALL_DIRECTORY}/mycodo/scripts/upgrade_commands.sh"
 LOG_LOCATION=${INSTALL_DIRECTORY}/install/setup.log
+UNAME_TYPE=$(uname -m)
+MACHINE_TYPE=$(dpkg --print-architecture)
+INFLUX_A='NONE'
+INFLUX_B='NONE'
 
 # Fix for below issue(s)
 # https://github.com/pypa/setuptools/issues/3278
@@ -15,33 +19,32 @@ LOG_LOCATION=${INSTALL_DIRECTORY}/install/setup.log
 export SETUPTOOLS_USE_DISTUTILS=stdlib
 
 if [ "$EUID" -ne 0 ]; then
-    printf "Must be run as root: \"sudo /bin/bash %s/install/setup.sh\"\n" "${INSTALL_DIRECTORY}"
+    printf "Error: Script must be run as root, \"sudo /bin/bash %s/install/setup.sh\"\n" "${INSTALL_DIRECTORY}"
     exit 1
 fi
 
 printf "Checking Python version...\n"
 if hash python3 2>/dev/null; then
   if ! python3 "${INSTALL_DIRECTORY}"/mycodo/scripts/upgrade_check.py --min_python_version "3.6"; then
-    printf "Incorrect Python version found. Mycodo requires Python >= 3.6.\n"
-    printf "If you're running Debian Stretch with Python 3.5, you will need to install at least Debian Buster with Python 3.7 to install the latest version of Mycodo.\n"
+    printf "Error: Incorrect Python version found. Mycodo requires Python >= 3.6.\n"
     exit 1
   else
-    printf "Python >= 3.6 found. Continuing with the install.\n"
+    printf "Python >= 3.6 found.\n"
   fi
 else
-  printf "\npython3 was not found. Cannot proceed with the install without python3 (Python >= 3.6).\n"
+  printf "\nError: correct python version not found. Python >= 3.6 required in PATH to proceed with the install.\n"
   exit 1
 fi
 
 WHIPTAIL=$(command -v whiptail)
 exitstatus=$?
 if [ $exitstatus != 0 ]; then
-    printf "\nwhiptail not installed. Install it with 'sudo apt-get install whiptail' then try the install again.\n"
+    printf "\nError: whiptail not installed. Install it then try the Mycodo setup again.\n"
     exit 1
 fi
 
-NOW=$(date)
-printf "### Mycodo installation initiated %s\n" "${NOW}" 2>&1 | tee -a "${LOG_LOCATION}"
+START_A=$(date)
+printf "### Mycodo installation initiated %s\n" "${START_A}" 2>&1 | tee -a "${LOG_LOCATION}"
 
 clear
 LICENSE=$(whiptail --title "Mycodo Installer: License Agreement" \
@@ -71,16 +74,10 @@ LANGUAGE=$(whiptail --title "Mycodo Installer" \
                   3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus != 0 ]; then
-    printf "Mycodo install canceled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
+    printf "Mycodo install cancelled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
     exit 1
 else
     echo "${LANGUAGE}" > "${INSTALL_DIRECTORY}/.language"
-fi
-
-exitstatus=$?
-if [ $exitstatus != 0 ]; then
-    printf "Mycodo install canceled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
-    exit 1
 fi
 
 clear
@@ -91,45 +88,42 @@ INSTALL=$(whiptail --title "Mycodo Installer: Install" \
                    3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus != 0 ]; then
-    printf "Mycodo install canceled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
+    printf "Mycodo install cancelled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
     exit 1
 fi
-
-UNAME_TYPE=$(uname -m)
-MACHINE_TYPE=$(dpkg --print-architecture)
 
 clear
 if [[ ${MACHINE_TYPE} == 'armhf' ]]; then
-    INFLUX=$(whiptail --title "Mycodo Installer: Measurement Database" \
-                      --backtitle "Mycodo" \
-                      --menu "Install Influxdb?\n\nIf you do not install InfluxDB now, you will need to set the InfluxDB server/credential settings in the Configuration after Mycodo is installed." 20 68 4 \
-                      "1)" "Install Influxdb 1.x (default)" \
-                      "0)" "Do Not Install Influxdb" \
-                      3>&1 1>&2 2>&3)
+    INFLUX_A=$(whiptail --title "Mycodo Installer: Measurement Database" \
+                        --backtitle "Mycodo" \
+                        --menu "Install Influxdb?\n\nIf you do not install InfluxDB now, you will need to set the InfluxDB server/credential settings in the Configuration after Mycodo is installed." 20 68 4 \
+                        "0)" "Install Influxdb 1.x (default)" \
+                        "1)" "Do Not Install Influxdb" \
+                        3>&1 1>&2 2>&3)
     exitstatus=$?
     if [ $exitstatus != 0 ]; then
-        printf "Mycodo install canceled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
+        printf "Mycodo install cancelled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
         exit 1
     fi
 elif [[ ${MACHINE_TYPE} == 'arm64' || ${UNAME_TYPE} == 'x86_64' ]]; then
-    INFLUX=$(whiptail --title "Mycodo Installer: Measurement Database" \
-                      --backtitle "Mycodo" \
-                      --menu "Install Influxdb?\n\nIf you do not install InfluxDB now, you will need to configure the InfluxDB server settings and credentials after Mycodo is installed. If using a 32-bit CPU, you can only use 1.x (do not use 2.x, as it only works with 64-bit CPUs)." 20 68 4 \
-                      "0)" "Install Influxdb 2.x (recommended)" \
-                      "1)" "Install Influxdb 1.x (old)" \
-                      "2)" "Do Not Install Influxdb" \
-                      3>&1 1>&2 2>&3)
+    INFLUX_B=$(whiptail --title "Mycodo Installer: Measurement Database" \
+                        --backtitle "Mycodo" \
+                        --menu "Install Influxdb?\n\nIf you do not install InfluxDB now, you will need to configure the InfluxDB server settings and credentials after Mycodo is installed. If using a 32-bit CPU, you can only use 1.x (do not use 2.x, as it only works with 64-bit CPUs)." 20 68 4 \
+                        "0)" "Install Influxdb 2.x (recommended)" \
+                        "1)" "Install Influxdb 1.x (old)" \
+                        "2)" "Do Not Install Influxdb" \
+                        3>&1 1>&2 2>&3)
     exitstatus=$?
     if [ $exitstatus != 0 ]; then
-        printf "Mycodo install canceled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
+        printf "Mycodo install cancelled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
         exit 1
     fi
 else
-    printf "\nCould not detect architecture\n"
+    printf "\nError: Could not detect architecture\n"
     exit 1
 fi
 
-if [[ ${INFLUX} == '2)' ]]; then
+if [[ ${INFLUX_A} == '1)' || ${INFLUX_B} == '2)' ]]; then
     clear
     INSTALL=$(whiptail --title "Mycodo Installer: Measurement Database" \
                        --backtitle "Mycodo" \
@@ -138,13 +132,13 @@ if [[ ${INFLUX} == '2)' ]]; then
                        3>&1 1>&2 2>&3)
     exitstatus=$?
     if [ $exitstatus != 0 ]; then
-        printf "Mycodo install canceled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
+        printf "Mycodo install cancelled by user\n" 2>&1 | tee -a "${LOG_LOCATION}"
         exit 1
     fi
 fi
 
-if [[ ${INFLUX} == 'NONE' ]]; then
-    printf "\nInflux install option not selected\n"
+if [[ ${INFLUX_A} == 'NONE' && ${INFLUX_B} == 'NONE' ]]; then
+    printf "\nError: Influx install option not selected\n"
     exit 1
 fi
 
@@ -175,8 +169,8 @@ set -e
 
 clear
 SECONDS=0
-NOW=$(date)
-printf "#### Mycodo installation began %s\n" "${NOW}" 2>&1 | tee -a "${LOG_LOCATION}"
+START_B=$(date)
+printf "#### Mycodo installation began %s\n" "${START_B}" 2>&1 | tee -a "${LOG_LOCATION}"
 
 ${INSTALL_CMD} update-swap-size 2>&1 | tee -a "${LOG_LOCATION}"
 ${INSTALL_CMD} update-apt 2>&1 | tee -a "${LOG_LOCATION}"
@@ -186,13 +180,13 @@ ${INSTALL_CMD} setup-virtualenv 2>&1 | tee -a "${LOG_LOCATION}"
 ${INSTALL_CMD} update-pip3 2>&1 | tee -a "${LOG_LOCATION}"
 ${INSTALL_CMD} update-pip3-packages 2>&1 | tee -a "${LOG_LOCATION}"
 ${INSTALL_CMD} install-wiringpi 2>&1 | tee -a "${LOG_LOCATION}"
-if [[ ${INFLUX} == '0)' ]]; then
+if [[ ${INFLUX_B} == '0)' ]]; then
     ${INSTALL_CMD} update-influxdb-2 2>&1 | tee -a "${LOG_LOCATION}"
     ${INSTALL_CMD} update-influxdb-2-db-user 2>&1 | tee -a "${LOG_LOCATION}"
-elif [[ ${INFLUX} == '1)' ]]; then
+elif [[ ${INFLUX_A} == '0)' || ${INFLUX_B} == '1)' ]]; then
     ${INSTALL_CMD} update-influxdb-1 2>&1 | tee -a "${LOG_LOCATION}"
     ${INSTALL_CMD} update-influxdb-1-db-user 2>&1 | tee -a "${LOG_LOCATION}"
-elif [[ ${INFLUX} == '2)' ]]; then
+elif [[ ${INFLUX_A} == '1)' || ${INFLUX_B} == '2)' ]]; then
     printf "Instructed to not install InfluxDB/n"
 fi
 ${INSTALL_CMD} initialize 2>&1 | tee -a "${LOG_LOCATION}"
@@ -216,8 +210,8 @@ if [[ -z ${IP} ]]; then
   IP="your.IP.address.here"
 fi
 
-CURRENT_DATE=$(date)
-printf "#### Mycodo Installer finished %s\n" "${CURRENT_DATE}" 2>&1 | tee -a "${LOG_LOCATION}"
+END=$(date)
+printf "#### Mycodo Installer finished %s\n" "${END}" 2>&1 | tee -a "${LOG_LOCATION}"
 
 DURATION=$SECONDS
 printf "#### Total install time: %d minutes and %d seconds\n" "$((DURATION / 60))" "$((DURATION % 60))" 2>&1 | tee -a "${LOG_LOCATION}"
